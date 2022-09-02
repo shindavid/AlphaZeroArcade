b8b7549e9f680d2b4da4a097bb3f8345849d6710
David Shin <shin.david@gmail.com>
2022-09-02 13:54:14 -0500

    csv->pickle, stop deduplicating data, make input more nnet-friendly, include starting position

--- py/connect4/generate_training_data.py
+++ py/connect4/generate_training_data.py
@@ -2,6 +2,7 @@
 import argparse
 import numpy as np
 import os
+import pickle
 import random
 import subprocess
 from tqdm import tqdm
@@ -13,7 +14,7 @@ def get_args():
     parser.add_argument("-c", '--c4-solver-dir', required=True,
                         help="base directory containing c4solver bin and 7x6.book")
     parser.add_argument("-n", "--num-training-games", type=int, help="number of training games")
-    parser.add_argument("-o", "--output-file", default="output.csv")
+    parser.add_argument("-o", "--output-file", default="output.pkl")
 
     return parser.parse_args()
 
@@ -31,15 +32,42 @@ def main():
     proc = subprocess.Popen(c4_cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
                             stderr=subprocess.PIPE, encoding='utf-8')
 
-    recorded_hashes = set()
     record_count = 0
-    skip_count = 0
 
-    with open(args.output_file, 'w') as output_file:
+    with open(args.output_file, 'wb') as output_file:
+        full_output = []
+
         for _ in tqdm(range(args.num_training_games), desc="Writing training games"):
             g = game.Game()
+            game_output = []
             move_history = ''
             while True:
+                record_count += 1
+
+                proc.stdin.write(move_history + '\n')
+                proc.stdin.flush()
+                stdout = proc.stdout.readline()
+
+                move_scores = list(map(int, stdout.split()[-7:]))
+
+                best_score = max(move_scores)
+                best_move_arr = (np.array(move_scores) == best_score).astype(int)
+                cur_player_value = np.sign(best_score) * .5 + .5
+                other_player_value = 1 - cur_player_value
+
+                gvec = g.vectorize()
+                data = (gvec, cur_player_value, best_move_arr)
+                game_output.append(data)
+
+                #
+                # value_vec = np.array([0.0, 0.0])
+                # value_vec[g.current_player] = cur_player_value
+                # value_vec[1 - g.current_player] = other_player_value
+                #
+                # output_vec = np.concatenate((gvec, best_move_arr, value_vec))
+                # output_file.write(' '.join(map(lambda s: '%g' % s, output_vec)))
+                # output_file.write('\n')
+
                 moves = g.get_valid_moves()
                 if not moves:
                     break
@@ -49,37 +77,10 @@ def main():
                     break
 
                 move_history += str(move)
-                gvec = g.vectorize()
-                gvec_hash = hash(move_history)
-                if gvec_hash in recorded_hashes:
-                    skip_count += 1
-                    continue
-                record_count += 1
-                recorded_hashes.add(gvec_hash)
 
-                proc.stdin.write(move_history + '\n')
-                proc.stdin.flush()
-                stdout = proc.stdout.readline()
+            full_output.append(game_output)
+        pickle.dump(full_output, output_file)
 
-                c4_line_output = list(map(int, stdout.split()))
-                assert len(c4_line_output) == 8
-                # assert c4_line_output[0] == move_history
-
-                move_scores = c4_line_output[1:]
-                best_score = max(move_scores)
-                best_move_arr = (np.array(move_scores) == best_score).astype(int)
-                cur_player_value = np.sign(best_score) * .5 + .5
-                other_player_value = 1 - cur_player_value
-
-                value_vec = np.array([0.0, 0.0])
-                value_vec[g.current_player] = cur_player_value
-                value_vec[1 - g.current_player] = other_player_value
-
-                output_vec = np.concatenate((gvec, best_move_arr, value_vec))
-                output_file.write(' '.join(map(lambda s: '%g' % s, output_vec)))
-                output_file.write('\n')
-
-    print('skip: ', skip_count)
     print('record: ', record_count)
 
 
