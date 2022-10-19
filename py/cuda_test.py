fd6370831c9a9d51304d0ccff242506b33252494
David Shin <shin.david@gmail.com>
2022-10-19 15:18:47 -0500

    do something nontrivial with model outputs

--- py/cuda_test.py
+++ py/cuda_test.py
@@ -8,17 +8,18 @@ import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-USE_LOADER = False
+USE_LOADER = True
+torch.set_grad_enabled(False)
 
-
-for batch_size in (1, 64):
-    for use_cuda in (False, True):
+for use_cuda in (True, False):
+    for batch_size in (1, 64):
         model = models.resnet50()
+        model.eval()
         size = 1024
 
         if USE_LOADER:
             dataset = datasets.FakeData(
-                size=1000,
+                size=size,
                 transform=transforms.ToTensor())
             loader = DataLoader(
                 dataset,
@@ -32,14 +33,15 @@ for batch_size in (1, 64):
         if use_cuda:
             model.to('cuda')
 
+        x = 0
         t1 = time.time()
         if use_cuda:
             for data, _ in loader:
                 data = data.to('cuda', non_blocking=True)
-                model(data)
+                x += torch.sum(model(data).to('cpu'))
         else:
             for data, _ in loader:
-                model(data)
+                x += torch.sum(model(data))
 
         t2 = time.time()
         t = t2 - t1
