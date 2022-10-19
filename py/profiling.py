c7bcf7088d9035557d04e6814aac444d22ece1cc
David Shin <shin.david@gmail.com>
2022-10-19 16:33:30 -0500

    profiler api tweak

--- py/profiling.py
+++ py/profiling.py
@@ -12,15 +12,18 @@ class Profiler:
         self._min = 9999999999
         self._max = 0
 
-    def start(self):
-        self._start_t = time.time()
+    def start(self, t=None):
+        t = time.time() if t is None else t
+        self._start_t = t
 
     def stop(self):
-        delta = time.time() - self._start_t
+        t = time.time()
+        delta = t - self._start_t
         self._total_sec += delta
         self._count += 1
         self._min = min(self._min, delta)
         self._max = max(self._max, delta)
+        return t
 
     def get_total_sec(self):
         return self._total_sec
