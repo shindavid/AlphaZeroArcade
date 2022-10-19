29827939d278f64409bffd2b8f3233db6d743948
David Shin <shin.david@gmail.com>
2022-10-19 14:39:51 -0500

    cuda test: no DataLoader to better mimic c4 usage

--- py/cuda_test.py
+++ py/cuda_test.py
@@ -1,24 +1,33 @@
+#!/usr/bin/env python3
 import time
 
+import torch
 from torch.utils.data import DataLoader
 
 import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+USE_LOADER = False
+
+
 for batch_size in (1, 64):
     for use_cuda in (False, True):
         model = models.resnet50()
+        size = 1024
 
-        dataset = datasets.FakeData(
-            size=1000,
-            transform=transforms.ToTensor())
-        loader = DataLoader(
-            dataset,
-            batch_size=1,
-            num_workers=1,
-            pin_memory=True
-        )
+        if USE_LOADER:
+            dataset = datasets.FakeData(
+                size=1000,
+                transform=transforms.ToTensor())
+            loader = DataLoader(
+                dataset,
+                batch_size=1,
+                num_workers=1,
+                pin_memory=True
+            )
+        else:
+            loader = [(torch.rand(batch_size, 3, 224, 224), None) for _ in range(size // batch_size)]
 
         if use_cuda:
             model.to('cuda')
