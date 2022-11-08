e7f16dd8d1c17699293c027e63285b490d2b1eec
David Shin <shin.david@gmail.com>
2022-11-08 12:00:51 -0600

    debug/release builds

--- py/build.py
+++ py/build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import argparse
 import os
 import sys
 
@@ -12,34 +12,63 @@ def run(cmd):
         sys.exit(1)
 
 
-torch_dir = Config.instance().get('libtorch_dir')
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", '--debug', action='store_true', help='debug build')
+    return parser.parse_args()
 
-repo_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-cfg = Config.instance().filename
-torch_link = 'https://pytorch.org/get-started/locally/'
-assert torch_dir, (f'Please download torch for C++ from {torch_link}, unzip to a path LIBTORCH_PATH, and then add an '
-                   f'entry "libtorch_dir=LIBTORCH_PATH" to {cfg}. '
-                   'An explicitly downloaded libtorch must be used instead of the one that ships with conda, '
-                   'due to the cxx11-abi issue. See here: https://github.com/pytorch/pytorch/issues/17492')
-assert os.path.isdir(torch_dir)
 
-conda_prefix = os.environ.get('CONDA_PREFIX', None)
-assert conda_prefix, 'It appears you do not have a conda environment activated. Please activate!'
-eigen_dir = os.path.join(conda_prefix, 'share/eigen3/cmake')
-assert os.path.isdir(eigen_dir), 'Please conda install eigen.'
+def main():
+    args = get_args()
+    debug = bool(args.debug)
 
-lib_cmake_dir = os.path.join(conda_prefix, 'lib/cmake')
-assert os.path.isdir(lib_cmake_dir)
-found_boost = False
-for path in os.listdir(lib_cmake_dir):
-    if path.startswith('Boost-') and os.path.isdir(os.path.join(lib_cmake_dir, path)):
-        found_boost = True
-        break
-assert found_boost, 'Please conda install boost.'
+    torch_dir = Config.instance().get('libtorch_dir')
 
-os.chdir(repo_root)
-cmake_cmd = f'cmake CMakeLists.txt -Btarget -DMY_TORCH_DIR={torch_dir} -DCMAKE_PREFIX_PATH={conda_prefix}'
-run(cmake_cmd)
+    repo_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    cfg = Config.instance().filename
+    torch_link = 'https://pytorch.org/get-started/locally/'
+    assert torch_dir, (f'Please download torch for C++ from {torch_link}, unzip to a path LIBTORCH_PATH, and then add an '
+                       f'entry "libtorch_dir=LIBTORCH_PATH" to {cfg}. '
+                       'An explicitly downloaded libtorch must be used instead of the one that ships with conda, '
+                       'due to the cxx11-abi issue. See here: https://github.com/pytorch/pytorch/issues/17492')
+    assert os.path.isdir(torch_dir)
 
-os.chdir('target')
-run('make')
+    conda_prefix = os.environ.get('CONDA_PREFIX', None)
+    assert conda_prefix, 'It appears you do not have a conda environment activated. Please activate!'
+    eigen_dir = os.path.join(conda_prefix, 'share/eigen3/cmake')
+    assert os.path.isdir(eigen_dir), 'Please conda install eigen.'
+
+    lib_cmake_dir = os.path.join(conda_prefix, 'lib/cmake')
+    assert os.path.isdir(lib_cmake_dir)
+    found_boost = False
+    for path in os.listdir(lib_cmake_dir):
+        if path.startswith('Boost-') and os.path.isdir(os.path.join(lib_cmake_dir, path)):
+            found_boost = True
+            break
+    assert found_boost, 'Please conda install boost.'
+
+    os.chdir(repo_root)
+
+    build_name = 'Debug' if debug else 'Release'
+    target_dir = f'target/{build_name}'
+    cmake_cmd_tokens = [
+        'cmake',
+        'CMakeLists.txt',
+        f'-B{target_dir}',
+        f'-DMY_TORCH_DIR={torch_dir}',
+        f'-DCMAKE_PREFIX_PATH={conda_prefix}'
+    ]
+    if debug:
+        cmake_cmd_tokens.append('-DCMAKE_BUILD_TYPE=Debug')
+    cmake_cmd = ' '.join(cmake_cmd_tokens)
+    run(cmake_cmd)
+
+    os.chdir(target_dir)
+    run('make')
+    bin_postfix = 'd' if debug else ''
+    bin_loc = os.path.join(repo_root, target_dir, 'bin', f'c4_gen_training_data{bin_postfix}')
+    print(f'Binary location: {bin_loc}')
+
+
+if __name__ == '__main__':
+    main()
