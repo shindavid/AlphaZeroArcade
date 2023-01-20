43b77b0b8836415e25e6bfa969345e1e5b849d9a
David Shin <shin.david@gmail.com>
2023-01-20 09:18:14 -0600

    add missing inline keyword

--- cpp/util/BoostUtil.hpp
+++ cpp/util/BoostUtil.hpp
@@ -20,18 +20,18 @@ namespace program_options {
  *
  * boost::program_options::value<float>(&f)->default_value(f, util::create_string("%.3f", f))
  */
-auto float_value(const char* fmt, float* dest, float default_value) {
+inline auto float_value(const char* fmt, float* dest, float default_value) {
   std::string s = util::create_string(fmt, default_value);
   return boost::program_options::value<float>(dest)->default_value(default_value, s);
 }
 
-auto float_value(const char* fmt, float* dest) { return float_value(fmt, dest, *dest); }
+inline auto float_value(const char* fmt, float* dest) { return float_value(fmt, dest, *dest); }
 
 /*
  * abbrev_str(true, "abc", "a") == "abc,a"
  * abbrev_str(false, "abc", "a") == "abc"
  */
-std::string abbrev_str(bool abbreviate, const char* full_name, const char* abbreviation) {
+inline std::string abbrev_str(bool abbreviate, const char* full_name, const char* abbreviation) {
   return abbreviate ? util::create_string("%s,%s", full_name, abbreviation) : full_name;
 }
 
