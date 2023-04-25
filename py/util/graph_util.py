373e8d221b726adfaf951693571cf7039a445f83
David Shin <shin.david@gmail.com>
2023-04-25 11:36:47 -0500

    tarjans algorithm implementation

--- py/util/graph_util.py
+++ py/util/graph_util.py
@@ -45,3 +45,52 @@ def direct_children(G: AdjMatrix) -> AdjMatrix:
                 C[u, v] = False
                 break
     return C
+
+
+def tarjans_algorithm(G: AdjMatrix) -> np.ndarray:
+    """
+    Computes the strongly connected components of a graph using Tarjan's algorithm.
+
+    Outputs an array A such that A[i] is the index of the component containing node i.
+    """
+    n = G.shape[0]
+    assert G.shape == (n, n)
+
+    index = 0
+    stack = []
+    indices = np.full(n, -1)
+    lowlinks = np.full(n, -1)
+    on_stack = np.full(n, False)
+    next_component_index = 0
+    component_indices = np.full(n, -1)
+
+    def strong_connect(v):
+        nonlocal index, next_component_index
+        indices[v] = index
+        lowlinks[v] = index
+        index += 1
+        stack.append(v)
+        on_stack[v] = True
+
+        for w in np.where(G[v])[0]:
+            if indices[w] == -1:
+                strong_connect(w)
+                lowlinks[v] = min(lowlinks[v], lowlinks[w])
+            elif on_stack[w]:
+                lowlinks[v] = min(lowlinks[v], indices[w])
+
+        if lowlinks[v] == indices[v]:
+            w = stack.pop()
+            on_stack[w] = False
+            component_indices[w] = next_component_index
+            while w != v:
+                w = stack.pop()
+                on_stack[w] = False
+                component_indices[w] = next_component_index
+            next_component_index += 1
+
+    for v in range(n):
+        if indices[v] == -1:
+            strong_connect(v)
+
+    return component_indices
