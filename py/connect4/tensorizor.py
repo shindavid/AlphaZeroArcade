c1c78dda306db971cf05fd61dd81bfdf05f196e9
David Shin <shin.david@gmail.com>
2023-05-04 09:13:33 -0500

    make resnet modules reusable for other games (part 1)

--- py/connect4/tensorizor.py
+++ py/connect4/tensorizor.py
@@ -3,132 +3,17 @@ from typing import List
 import numpy as np
 import torch
 from torch import nn as nn
-from torch.nn import functional as F
 
 from interface import NeuralNetworkInput, ActionIndex, AbstractGameTensorizor, AbstractSymmetryTransform, \
     IdentityTransform, PolicyTensor
 from neural_net import NeuralNet
 from profiling import ProfilerRegistry
 from connect4.game_logic import C4GameState, NUM_COLUMNS, NUM_ROWS, NUM_COLORS, Color, MAX_MOVES_PER_GAME
+from res_net_modules import ConvBlock, ResBlock, PolicyHead, ValueHead
 from util.torch_util import Shape
 
 
 # torch.set_default_dtype(torch.float64)
-
-
-class ConvBlock(nn.Module):
-    """
-    From "Mastering the Game of Go without Human Knowledge" (AlphaGo Zero paper):
-
-    The convolutional block applies the following modules:
-
-    1. A convolution of 256 filters of kernel size 3 × 3 with stride 1
-    2. Batch normalisation
-    3. A rectifier non-linearity
-
-    https://discovery.ucl.ac.uk/id/eprint/10045895/1/agz_unformatted_nature.pdf
-    """
-    def __init__(self, n_input_channels: int, n_conv_filters: int):
-        super(ConvBlock, self).__init__()
-        self.conv = nn.Conv2d(n_input_channels, n_conv_filters, kernel_size=3, stride=1, padding=1, bias=False)
-        self.batch = nn.BatchNorm2d(n_conv_filters)
-
-    def forward(self, x):
-        return F.relu(self.batch(self.conv(x)))
-
-
-class ResBlock(nn.Module):
-    """
-    From "Mastering the Game of Go without Human Knowledge" (AlphaGo Zero paper):
-
-    Each residual block applies the following modules sequentially to its input:
-
-    1. A convolution of 256 filters of kernel size 3 × 3 with stride 1
-    2. Batch normalisation
-    3. A rectifier non-linearity
-    4. A convolution of 256 filters of kernel size 3 × 3 with stride 1
-    5. Batch normalisation
-    6. A skip connection that adds the input to the block
-    7. A rectifier non-linearity
-
-    https://discovery.ucl.ac.uk/id/eprint/10045895/1/agz_unformatted_nature.pdf
-    """
-    def __init__(self, n_conv_filters: int):
-        super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(n_conv_filters, n_conv_filters, kernel_size=3, stride=1, padding=1, bias=False)
-        self.batch1 = nn.BatchNorm2d(n_conv_filters)
-        self.conv2 = nn.Conv2d(n_conv_filters, n_conv_filters, kernel_size=3, stride=1, padding=1, bias=False)
-        self.batch2 = nn.BatchNorm2d(n_conv_filters)
-
-    def forward(self, x):
-        identity = x
-        out = F.relu(self.batch1(self.conv1(x)))
-        out = self.batch2(self.conv2(out))
-        out += identity  # skip connection
-        return F.relu(out)
-
-
-class PolicyHead(nn.Module):
-    """
-    From "Mastering the Game of Go without Human Knowledge" (AlphaGo Zero paper):
-
-    The policy head applies the following modules:
-
-    1. A convolution of 2 filters of kernel size 1 × 1 with stride 1
-    2. Batch normalisation
-    3. A rectifier non-linearity
-    4. A fully connected linear layer that outputs a vector of size 19^2 + 1 = 362 corresponding to
-    logit probabilities for all intersections and the pass move
-
-    https://discovery.ucl.ac.uk/id/eprint/10045895/1/agz_unformatted_nature.pdf
-    """
-    def __init__(self, n_input_channels: int):
-        super(PolicyHead, self).__init__()
-        self.conv = nn.Conv2d(n_input_channels, 2, kernel_size=1, stride=1, bias=False)
-        self.batch = nn.BatchNorm2d(2)
-        self.linear = nn.Linear(2 * NUM_COLUMNS * NUM_ROWS, NUM_COLUMNS)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.batch(x)
-        x = F.relu(x)
-        x = x.view(-1, 2 * NUM_COLUMNS * NUM_ROWS)
-        x = self.linear(x)
-        return x
-
-
-class ValueHead(nn.Module):
-    """
-    From "Mastering the Game of Go without Human Knowledge" (AlphaGo Zero paper):
-
-    The value head applies the following modules:
-
-    1. A convolution of 1 filter of kernel size 1 × 1 with stride 1
-    2. Batch normalisation
-    3. A rectifier non-linearity
-    4. A fully connected linear layer to a hidden layer of size 256
-    5. A rectifier non-linearity
-    6. A fully connected linear layer to a scalar
-    7. A tanh non-linearity outputting a scalar in the range [−1, 1]
-
-    https://discovery.ucl.ac.uk/id/eprint/10045895/1/agz_unformatted_nature.pdf
-
-    Here, we are choosing to replace the scalar with a length-p array to generalize for p-player games. The output
-    will be interpreted as logit probabilities for the corresponding player's expected win shares.
-    """
-    def __init__(self, n_input_channels: int):
-        super(ValueHead, self).__init__()
-        self.conv = nn.Conv2d(n_input_channels, 1, kernel_size=1, stride=1, bias=False)
-        self.batch = nn.BatchNorm2d(1)
-        self.linear1 = nn.Linear(NUM_COLUMNS * NUM_ROWS, 256)
-        self.linear2 = nn.Linear(256, 2)
-
-    def forward(self, x):
-        x = F.relu(self.batch(self.conv(x)))
-        x = x.view(-1, NUM_COLUMNS * NUM_ROWS)
-        x = F.relu(self.linear1(x))
-        x = self.linear2(x)
-        return x
 
 
 class C4Net(NeuralNet):
