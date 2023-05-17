e3a103d907d94fc5f6b18ed6c68b96157a6b53f9
David Shin <shin.david@gmail.com>
2023-05-17 16:00:16 -0500

    rm outdated comment

--- cpp/common/AbstractSymmetryTransform.hpp
+++ cpp/common/AbstractSymmetryTransform.hpp
@@ -9,11 +9,6 @@ namespace common {
 /*
  * SymmetryTransform's provide a mechanism to effectively apply a symmetry transform to a GameState. In the game of go,
  * there are 8 transforms: 4 rotations and 4 reflections. See AlphaGo papers for more details.
- *
- * NOTE: some derived classes will invoke certain torch functions that are likely to result in dynamic memory
- * allocation (such as Tensor.flip()). In principle, by supplying "scratch" memory space, we should be able to avoid
- * dynamic memory allocation. This represents an inefficiency that deserves to be fixed. Doing so may require modifying
- * the interface of this class. I am holding off on doing this until I better understand the torch c++ API.
  */
 template<typename GameState, typename Tensorizor>
 class AbstractSymmetryTransform {
