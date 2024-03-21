8824941a78ab3362defba5b65555305a39e56b2f
David Shin <shin.david@gmail.com>
2024-03-21 09:50:05 -0500

    --rating-block-rate option

--- py/alphazero/servers/loop_control/params.py
+++ py/alphazero/servers/loop_control/params.py
@@ -9,6 +9,7 @@ class LoopControllerParams:
     port: int = constants.DEFAULT_LOOP_CONTROLLER_PORT
     cuda_device: str = 'cuda:0'
     model_cfg: str = 'default'
+    rating_block_rate: int = 10
 
     @staticmethod
     def create(args) -> 'LoopControllerParams':
@@ -16,6 +17,7 @@ class LoopControllerParams:
             port=args.port,
             cuda_device=args.cuda_device,
             model_cfg=args.model_cfg,
+            rating_block_rate=args.rating_block_rate,
         )
 
     @staticmethod
@@ -31,6 +33,14 @@ class LoopControllerParams:
                            help='cuda device used for network training (default: %(default)s)')
         group.add_argument('-m', '--model-cfg', default=defaults.model_cfg,
                            help='model config (default: %(default)s)')
+        group.add_argument('-r', '--rating-block-rate', type=int,
+                           default=defaults.rating_block_rate,
+                           help='if at least one rating server is using a dedicated GPU, then '
+                           'this parameter is ignored. Otherwise, this parameter is used to '
+                           'prevent rating servers from getting starved by self-play/training. '
+                           'If at least this many generations of self-play have happened since a '
+                           'rating server last got to run, then one rating server is temporarily '
+                           'elevated in priority over self-play/training. (default: %(default)s)')
 
     def add_to_cmd(self, cmd: List[str]):
         defaults = LoopControllerParams()
@@ -40,3 +50,5 @@ class LoopControllerParams:
             cmd.extend(['--cuda-device', self.cuda_device])
         if self.model_cfg != defaults.model_cfg:
             cmd.extend(['--model-cfg', self.model_cfg])
+        if self.rating_block_rate != defaults.rating_block_rate:
+            cmd.extend(['--rating-block-rate', str(self.rating_block_rate)])
