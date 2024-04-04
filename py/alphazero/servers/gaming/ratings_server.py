dc1066c58b60a6a730f9b31430b745562c56c72e
David Shin <shin.david@gmail.com>
2024-04-04 17:12:57 -0500

    rm base class from gaming servers, untangle dispatch flow

--- py/alphazero/servers/gaming/ratings_server.py
+++ py/alphazero/servers/gaming/ratings_server.py
@@ -1,14 +1,17 @@
 from alphazero.logic.custom_types import ClientRole
 from alphazero.logic.ratings import extract_match_record
+from alphazero.logic.shutdown_manager import ShutdownManager
 from alphazero.servers.gaming.base_params import BaseParams
-from alphazero.servers.gaming.game_server_base import GameServerBase
+from alphazero.servers.gaming.log_forwarder import LogForwarder
+from alphazero.servers.gaming.session_data import SessionData
 from util.logging_util import LoggingParams, get_logger
-from util.socket_util import JsonDict
+from util.socket_util import JsonDict, SocketRecvException, SocketSendException
 from util import subprocess_util
 
 from dataclasses import dataclass, fields
 import logging
 import threading
+import time
 
 
 logger = get_logger()
@@ -39,42 +42,70 @@ class RatingsServerParams(BaseParams):
                            help='parallelism factor (default: %(default)s)')
 
 
-class RatingsServer(GameServerBase):
+class RatingsServer:
     def __init__(self, params: RatingsServerParams, logging_params: LoggingParams):
-        super().__init__(params, logging_params, ClientRole.RATINGS_SERVER)
+        self._params = params
+        self._session_data = SessionData(params)
+        self._shutdown_manager = ShutdownManager()
+        self._log_forwarder = LogForwarder(self._shutdown_manager, logging_params)
         self._running = False
 
     def run(self):
         try:
             threading.Thread(target=self._main_loop, name='main_loop', daemon=True).start()
-            self.shutdown_manager.wait_for_shutdown_request()
+            self._shutdown_manager.wait_for_shutdown_request()
         except KeyboardInterrupt:
             logger.info('Caught Ctrl-C')
         finally:
-            self.shutdown_manager.shutdown()
+            self._shutdown_manager.shutdown()
 
     def _main_loop(self):
         try:
-            self.init_socket()
-            self.shutdown_manager.register(lambda: self.loop_controller_socket.close())
-            self.send_handshake()
-            self.recv_handshake()
+            self._init_socket()
+            self._send_handshake()
+            self._recv_handshake()
 
-            threading.Thread(target=self.recv_loop, daemon=True).start()
+            threading.Thread(target=self._recv_loop, daemon=True).start()
         except:
             logger.error('Unexpected error in main_loop():', exc_info=True)
-            self.shutdown_manager.request_shutdown(1)
+            self._shutdown_manager.request_shutdown(1)
 
-    def send_ready(self):
-        data = {
-            'type': 'ready',
-            }
-        self.loop_controller_socket.send_json(data)
+    def _init_socket(self):
+        self._session_data.init_socket()
+        self._log_forwarder.set_socket(self._session_data.socket)
+        self._shutdown_manager.register(lambda: self._session_data.socket.close())
 
-    def recv_loop_prelude(self):
-        self.send_ready()
+    def _send_handshake(self):
+        self._session_data.send_handshake(ClientRole.RATINGS_SERVER)
 
-    def handle_msg(self, msg: JsonDict) -> bool:
+    def _recv_handshake(self):
+        self._session_data.recv_handshake(ClientRole.RATINGS_SERVER, self._log_forwarder)
+
+    def _recv_loop(self):
+        try:
+            self._send_ready()
+            while True:
+                msg = self._session_data.socket.recv_json()
+                if self._handle_msg(msg):
+                    break
+        except SocketRecvException:
+            logger.warn('Encountered SocketRecvException in recv_loop(). '
+                        'Loop controller likely shut down.')
+            self._shutdown_manager.request_shutdown(0)
+        except SocketSendException:
+            # Include exc_info in send-case because it's a bit more unexpected
+            logger.warn('Encountered SocketSendException in recv_loop(). '
+                        'Loop controller likely shut down.', exc_info=True)
+            self._shutdown_manager.request_shutdown(0)
+        except:
+            logger.error(f'Unexpected error in recv_loop():', exc_info=True)
+            self._shutdown_manager.request_shutdown(1)
+
+    def _send_ready(self):
+        data = { 'type': 'ready', }
+        self._session_data.socket.send_json(data)
+
+    def _handle_msg(self, msg: JsonDict) -> bool:
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'ratings-server received json message: {msg}')
 
@@ -82,23 +113,27 @@ class RatingsServer(GameServerBase):
         if msg_type == 'match-request':
             self._handle_match_request(msg)
         elif msg_type == 'quit':
-            self.quit()
+            self._quit()
             return True
         else:
             raise Exception(f'Unknown message type: {msg_type}')
         return False
 
     def _handle_match_request(self, msg: JsonDict):
-        thread = threading.Thread(target=self._run_match, args=(msg,),
-                                  daemon=True, name=f'run-match')
+        thread = threading.Thread(target=self._run_match, args=(msg,), daemon=True,
+                                  name=f'run-match')
         thread.start()
+
+    def _quit(self):
+        logger.info(f'Received quit command')
+        self._shutdown_manager.request_shutdown(0)
 
     def _run_match(self, msg: JsonDict):
         try:
             self._run_match_helper(msg)
         except:
             logger.error(f'Unexpected error in run-match:', exc_info=True)
-            self.shutdown_manager.request_shutdown(1)
+            self._shutdown_manager.request_shutdown(1)
 
     def _run_match_helper(self, msg: JsonDict):
         assert not self._running
@@ -109,19 +144,19 @@ class RatingsServer(GameServerBase):
         n_games = msg['n_games']
         n_mcts_iters = msg['n_mcts_iters']
 
-        n_search_threads = self.params.n_search_threads
-        parallelism_factor = self.params.parallelism_factor
+        n_search_threads = self._params.n_search_threads
+        parallelism_factor = self._params.parallelism_factor
 
-        ps1 = self.get_mcts_player_str(mcts_gen, n_mcts_iters, n_search_threads)
-        ps2 = self.get_reference_player_str(ref_strength)
-        binary = self.binary_path
+        ps1 = self._get_mcts_player_str(mcts_gen, n_mcts_iters, n_search_threads)
+        ps2 = self._get_reference_player_str(ref_strength)
+        binary = self._session_data.binary_path
         cmd = [
             binary,
             '-G', n_games,
-            '--loop-controller-hostname', self.loop_controller_host,
-            '--loop-controller-port', self.loop_controller_port,
+            '--loop-controller-hostname', self._params.loop_controller_host,
+            '--loop-controller-port', self._params.loop_controller_port,
             '--client-role', ClientRole.RATINGS_WORKER.value,
-            '--cuda-device', self.cuda_device,
+            '--cuda-device', self._params.cuda_device,
             '--weights-request-generation', mcts_gen,
             '--do-not-report-metrics',
             '-p', parallelism_factor,
@@ -130,13 +165,14 @@ class RatingsServer(GameServerBase):
             ]
         cmd = ' '.join(map(str, cmd))
 
-        mcts_name = RatingsServer.get_mcts_player_name(mcts_gen)
-        ref_name = RatingsServer.get_reference_player_name(ref_strength)
+        mcts_name = RatingsServer._get_mcts_player_name(mcts_gen)
+        ref_name = RatingsServer._get_reference_player_name(ref_strength)
 
         proc = subprocess_util.Popen(cmd)
         logger.info(f'Running {mcts_name} vs {ref_name} match [{proc.pid}]: {cmd}')
         stdout_buffer = []
-        self.forward_output('ratings-worker', proc, stdout_buffer, close_remote_log=False)
+        self._log_forwarder.forward_output(
+            'ratings-worker', proc, stdout_buffer, close_remote_log=False)
 
         # NOTE: extracting the match record from stdout is potentially fragile. Consider
         # changing this to have the c++ process directly communicate its win/loss data to the
@@ -153,33 +189,33 @@ class RatingsServer(GameServerBase):
             'ref_strength': ref_strength,
         }
 
-        self.loop_controller_socket.send_json(data)
-        self.send_ready()
+        self._session_data.socket.send_json(data)
+        self._send_ready()
 
     @staticmethod
-    def get_mcts_player_name(gen: int):
+    def _get_mcts_player_name(gen: int):
         return f'MCTS-{gen}'
 
-    def get_mcts_player_str(self, gen: int, n_mcts_iters: int, n_search_threads: int):
-        name = RatingsServer.get_mcts_player_name(gen)
+    def _get_mcts_player_str(self, gen: int, n_mcts_iters: int, n_search_threads: int):
+        name = RatingsServer._get_mcts_player_name(gen)
 
         player_args = [
             '--type=MCTS-C',
             '--name', name,
             '-i', n_mcts_iters,
             '-n', n_search_threads,
-            '--cuda-device', self.cuda_device,
+            '--cuda-device', self._params.cuda_device,
         ]
 
         return ' '.join(map(str, player_args))
 
     @staticmethod
-    def get_reference_player_name(strength: int):
+    def _get_reference_player_name(strength: int):
         return f'ref-{strength}'
 
-    def get_reference_player_str(self, strength: int):
-        name = RatingsServer.get_reference_player_name(strength)
-        family = self.game_spec.reference_player_family
+    def _get_reference_player_str(self, strength: int):
+        name = RatingsServer._get_reference_player_name(strength)
+        family = self._session_data.game_spec.reference_player_family
         type_str = family.type_str
         strength_param = family.strength_param
 
--- py/alphazero/servers/gaming/self_play_server.py
+++ py/alphazero/servers/gaming/self_play_server.py
@@ -1,13 +1,17 @@
 from alphazero.logic.custom_types import ClientRole
+from alphazero.logic.shutdown_manager import ShutdownManager
 from alphazero.servers.gaming.base_params import BaseParams
-from alphazero.servers.gaming.game_server_base import GameServerBase
+from alphazero.servers.gaming.log_forwarder import LogForwarder
+from alphazero.servers.gaming.session_data import SessionData
 from util.logging_util import LoggingParams, get_logger
-from util.socket_util import JsonDict
+from util.repo_util import Repo
+from util.socket_util import JsonDict, SocketRecvException, SocketSendException
 from util import subprocess_util
 
 from dataclasses import dataclass, fields
 import logging
 import threading
+import time
 
 
 logger = get_logger()
@@ -26,33 +30,66 @@ class SelfPlayServerParams(BaseParams):
         BaseParams.add_base_args(group)
 
 
-class SelfPlayServer(GameServerBase):
+class SelfPlayServer:
     def __init__(self, params: SelfPlayServerParams, logging_params: LoggingParams):
-        super().__init__(params, logging_params, ClientRole.SELF_PLAY_SERVER)
+        self._params = params
+        self._session_data = SessionData(params)
+        self._shutdown_manager = ShutdownManager()
+        self._log_forwarder = LogForwarder(self._shutdown_manager, logging_params)
         self._running = False
 
     def run(self):
         try:
             threading.Thread(target=self._main_loop, name='main_loop', daemon=True).start()
-            self.shutdown_manager.wait_for_shutdown_request()
+            self._shutdown_manager.wait_for_shutdown_request()
         except KeyboardInterrupt:
             logger.info('Caught Ctrl-C')
         finally:
-            self.shutdown_manager.shutdown()
+            self._shutdown_manager.shutdown()
 
     def _main_loop(self):
         try:
-            self.init_socket()
-            self.shutdown_manager.register(lambda: self.loop_controller_socket.close())
-            self.send_handshake()
-            self.recv_handshake()
+            self._init_socket()
+            self._send_handshake()
+            self._recv_handshake()
 
-            threading.Thread(target=self.recv_loop, daemon=True).start()
+            threading.Thread(target=self._recv_loop, daemon=True).start()
         except:
             logger.error('Unexpected error in main_loop():', exc_info=True)
-            self.shutdown_manager.request_shutdown(1)
+            self._shutdown_manager.request_shutdown(1)
 
-    def handle_msg(self, msg: JsonDict) -> bool:
+    def _init_socket(self):
+        self._session_data.init_socket()
+        self._log_forwarder.set_socket(self._session_data.socket)
+        self._shutdown_manager.register(lambda: self._session_data.socket.close())
+
+    def _send_handshake(self):
+        self._session_data.send_handshake(ClientRole.SELF_PLAY_SERVER)
+
+    def _recv_handshake(self):
+        self._session_data.recv_handshake(ClientRole.SELF_PLAY_SERVER, self._log_forwarder)
+
+    def _recv_loop(self):
+        try:
+            self._send_ready()
+            while True:
+                msg = self._session_data.socket.recv_json()
+                if self._handle_msg(msg):
+                    break
+        except SocketRecvException:
+            logger.warn('Encountered SocketRecvException in recv_loop(). '
+                        'Loop controller likely shut down.')
+            self._shutdown_manager.request_shutdown(0)
+        except SocketSendException:
+            # Include exc_info in send-case because it's a bit more unexpected
+            logger.warn('Encountered SocketSendException in recv_loop(). '
+                        'Loop controller likely shut down.', exc_info=True)
+            self._shutdown_manager.request_shutdown(0)
+        except:
+            logger.error(f'Unexpected error in recv_loop():', exc_info=True)
+            self._shutdown_manager.request_shutdown(1)
+
+    def _handle_msg(self, msg: JsonDict) -> bool:
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'self-play-server received json message: {msg}')
 
@@ -62,29 +99,35 @@ class SelfPlayServer(GameServerBase):
         elif msg_type == 'start':
             self._handle_start()
         elif msg_type == 'quit':
-            self.quit()
+            self._quit()
             return True
         else:
             raise Exception(f'Unknown message type: {msg_type}')
         return False
 
-    def recv_loop_prelude(self):
-        data = {
-            'type': 'ready',
-        }
-        self.loop_controller_socket.send_json(data)
+    def _send_ready(self):
+        data = {'type': 'ready', }
+        self._session_data.socket.send_json(data)
 
     def _handle_start_gen0(self, msg: JsonDict):
         thread = threading.Thread(target=self._start_gen0, args=(msg,),
                                   daemon=True, name=f'start-gen0')
         thread.start()
 
+    def _handle_start(self):
+        thread = threading.Thread(target=self._start, daemon=True, name=f'start')
+        thread.start()
+
+    def _quit(self):
+        logger.info(f'Received quit command')
+        self._shutdown_manager.request_shutdown(0)
+
     def _start_gen0(self, msg: JsonDict):
         try:
             self._start_gen0_helper(msg)
         except:
             logger.error(f'Error in start_gen0:', exc_info=True)
-            self.shutdown_manager.request_shutdown(1)
+            self._shutdown_manager.request_shutdown(1)
 
     def _start_gen0_helper(self, msg):
         assert not self._running
@@ -109,10 +152,10 @@ class SelfPlayServer(GameServerBase):
         ]
 
         self_play_cmd = [
-            self.binary_path,
+            self._session_data.binary_path,
             '-G', 0,
-            '--loop-controller-hostname', self.loop_controller_host,
-            '--loop-controller-port', self.loop_controller_port,
+            '--loop-controller-hostname', self._params.loop_controller_host,
+            '--loop-controller-port', self._params.loop_controller_port,
             '--client-role', ClientRole.SELF_PLAY_WORKER.value,
             '--do-not-report-metrics',
             '--player', '"%s"' % (' '.join(map(str, player_args))),
@@ -123,7 +166,7 @@ class SelfPlayServer(GameServerBase):
 
         proc = subprocess_util.Popen(self_play_cmd)
         logger.info(f'Running gen-0 self-play [{proc.pid}]: {self_play_cmd}')
-        self.forward_output('gen0-self-play-worker', proc)
+        self._log_forwarder.forward_output('gen0-self-play-worker', proc)
 
         logger.info(f'Gen-0 self-play complete!')
         self._running = False
@@ -131,18 +174,14 @@ class SelfPlayServer(GameServerBase):
         data = {
             'type': 'gen0-complete',
         }
-        self.loop_controller_socket.send_json(data)
-
-    def _handle_start(self):
-        thread = threading.Thread(target=self._start, daemon=True, name=f'start')
-        thread.start()
+        self._session_data.socket.send_json(data)
 
     def _start(self):
         try:
             self._start_helper()
         except:
             logger.error(f'Error in start:', exc_info=True)
-            self.shutdown_manager.request_shutdown(1)
+            self._shutdown_manager.request_shutdown(1)
 
     def _start_helper(self):
         assert not self._running
@@ -151,7 +190,7 @@ class SelfPlayServer(GameServerBase):
         player_args = [
             '--type=MCTS-T',
             '--name=MCTS',
-            '--cuda-device', self.cuda_device,
+            '--cuda-device', self._params.cuda_device,
         ]
 
         player2_args = [
@@ -160,12 +199,12 @@ class SelfPlayServer(GameServerBase):
         ]
 
         self_play_cmd = [
-            self.binary_path,
+            self._session_data.binary_path,
             '-G', 0,
-            '--loop-controller-hostname', self.loop_controller_host,
-            '--loop-controller-port', self.loop_controller_port,
+            '--loop-controller-hostname', self._params.loop_controller_host,
+            '--loop-controller-port', self._params.loop_controller_port,
             '--client-role', ClientRole.SELF_PLAY_WORKER.value,
-            '--cuda-device', self.cuda_device,
+            '--cuda-device', self._params.cuda_device,
             '--player', '"%s"' % (' '.join(map(str, player_args))),
             '--player', '"%s"' % (' '.join(map(str, player2_args))),
         ]
@@ -174,5 +213,5 @@ class SelfPlayServer(GameServerBase):
 
         proc = subprocess_util.Popen(self_play_cmd)
         logger.info(f'Running self-play [{proc.pid}]: {self_play_cmd}')
-        self.forward_output('self-play-worker', proc)
+        self._log_forwarder.forward_output('self-play-worker', proc)
         assert False, 'Should not get here'
