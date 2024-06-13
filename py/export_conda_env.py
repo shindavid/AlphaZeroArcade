25ede00bf2ed509b564f18a629b0a45a7d5ae911
Rediness <driouiche.younes@gmail.com>
2024-06-12 22:02:54 -0400

    remove env specific info from env yaml file and add sqlite3 lib

--- py/export_conda_env.py
+++ py/export_conda_env.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
 
-cmd = 'conda env export > environment.yml'
+# Remove the first line which contains the environments name
+# Remove the last line which contains the prefix
+cmd = 'conda env export | head -n -1 | tail -n +2 > environment.yml'
 
 print('Running from repo root:\n')
 print(cmd)
