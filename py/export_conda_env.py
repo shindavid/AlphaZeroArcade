acee515f280563d94ba09c00aa3eed11a98e92f9
Rediness <driouiche.younes@gmail.com>
2024-06-12 22:58:06 -0400

    check that we are removing lines with the right keys

--- py/export_conda_env.py
+++ py/export_conda_env.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 import os
+from subprocess import run
 
+
+export_env_cmd = "conda env export"
 # Remove the first line which contains the environments name
 # Remove the last line which contains the prefix
-cmd = 'conda env export | head -n -1 | tail -n +2 > environment.yml'
+export_stripped_env_cmd = f"{export_env_cmd} | head -n -1 | tail -n +2"
+# Ensure the diff is as expected
+export_out = run(
+    export_env_cmd,
+    capture_output=True, text=True, shell=True
+)
+lines = export_out.stdout.split()
+assert \
+    len(lines) >= 2 \
+    and lines[0] == 'name:'  \
+    and lines[-2] == 'prefix:', \
+    "environment file format is not supported"
 
+
+cmd = f'{export_stripped_env_cmd} > environment.yml'
 print('Running from repo root:\n')
 print(cmd)
 repo_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
