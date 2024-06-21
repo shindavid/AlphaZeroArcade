8ede0a60475ff725939707fa1f33ba78578d6120
David Shin <shin.david@gmail.com>
2024-06-21 09:06:24 -0500

    shorter variable name

--- cpp/include/core/concepts/GameInputTensorizor.hpp
+++ cpp/include/core/concepts/GameInputTensorizor.hpp
@@ -8,16 +8,16 @@
 namespace core {
 namespace concepts {
 
-template <typename InputTensorizor, typename BaseState, typename FullState>
+template <typename IT, typename BaseState, typename FullState>
 concept GameInputTensorizor = requires(const BaseState& base_state1, const BaseState& base_state2,
                                        const FullState& full_state) {
-  requires eigen_util::concepts::FTensor<typename InputTensorizor::Tensor>;
-  requires util::concepts::UsableAsHashMapKey<typename InputTensorizor::EvalKey>;
-  requires util::concepts::UsableAsHashMapKey<typename InputTensorizor::MCTSKey>;
+  requires eigen_util::concepts::FTensor<typename IT::Tensor>;
+  requires util::concepts::UsableAsHashMapKey<typename IT::EvalKey>;
+  requires util::concepts::UsableAsHashMapKey<typename IT::MCTSKey>;
 
-  { InputTensorizor::eval_key(full_state) } -> std::same_as<typename InputTensorizor::EvalKey>;
-  { InputTensorizor::mcts_key(full_state) } -> std::same_as<typename InputTensorizor::MCTSKey>;
-  { InputTensorizor::tensorize(&base_state1, &base_state2) } -> std::same_as<typename InputTensorizor::Tensor>;
+  { IT::eval_key(full_state) } -> std::same_as<typename IT::EvalKey>;
+  { IT::mcts_key(full_state) } -> std::same_as<typename IT::MCTSKey>;
+  { IT::tensorize(&base_state1, &base_state2) } -> std::same_as<typename IT::Tensor>;
 };
 
 }  // namespace concepts
