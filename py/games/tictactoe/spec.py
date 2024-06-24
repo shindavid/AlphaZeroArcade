49a95e44ea683401a2b60b72561b867a9d42e96a
David Shin <shin.david@gmail.com>
2024-06-24 13:01:48 -0500

    tictactoe: bigger network

--- py/games/tictactoe/spec.py
+++ py/games/tictactoe/spec.py
@@ -11,16 +11,16 @@ NUM_PLAYERS = 2
 NUM_POSSIBLE_END_OF_GAME_SQUARE_STATES = NUM_PLAYERS + 1  # +1 for empty square
 
 
-def b3_c8(shape_info_dict: ShapeInfoDict):
+def b7_c32(shape_info_dict: ShapeInfoDict):
     input_shape = shape_info_dict['input'].shape
     board_shape = input_shape[1:]
     board_size = math.prod(board_shape)
     policy_shape = (NUM_ACTIONS, )
-    c_trunk = 8
-    c_mid = 8
+    c_trunk = 32
+    c_mid = 32
     c_policy_hidden = 2
     c_value_hidden = 1
-    n_value_hidden = 16
+    n_value_hidden = 256
 
     return ModelConfig(
         shape_info_dict=shape_info_dict,
@@ -31,6 +31,10 @@ def b3_c8(shape_info_dict: ShapeInfoDict):
             ModuleSpec(type='ResBlock', args=['block1', c_trunk, c_mid]),
             ModuleSpec(type='ResBlock', args=['block2', c_trunk, c_mid]),
             ModuleSpec(type='ResBlock', args=['block3', c_trunk, c_mid]),
+            ModuleSpec(type='ResBlock', args=['block4', c_trunk, c_mid]),
+            ModuleSpec(type='ResBlock', args=['block5', c_trunk, c_mid]),
+            ModuleSpec(type='ResBlock', args=['block6', c_trunk, c_mid]),
+            ModuleSpec(type='ResBlock', args=['block7', c_trunk, c_mid]),
             ],
 
         heads=[
@@ -52,8 +56,8 @@ def b3_c8(shape_info_dict: ShapeInfoDict):
 class TicTacToeSpec(GameSpec):
     name = 'tictactoe'
     model_configs = {
-        'default': b3_c8,
-        'b3_c8': b3_c8,
+        'default': b7_c32,
+        'b7_c32': b7_c32,
     }
     reference_player_family = ReferencePlayerFamily('Perfect', '--strength', 0, 1)
 
