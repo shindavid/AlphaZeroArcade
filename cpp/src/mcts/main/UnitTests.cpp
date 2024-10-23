198708a28090d4e1b5eb869567bed9151248cef8
Lichen Song <lichen.song.personal@gmail.com>
2024-10-23 17:31:26 -0500

    add print_tree test

--- cpp/src/mcts/main/UnitTests.cpp
+++ cpp/src/mcts/main/UnitTests.cpp
@@ -29,6 +29,8 @@ class ManagerTest : public testing::Test {
   using edge_t = mcts::Node<Game>::edge_t;
   using LookupTable = mcts::Node<Game>::LookupTable;
   using node_pool_index_t = mcts::Node<Game>::node_pool_index_t;
+  using edge_pool_index_t = mcts::Node<Game>::edge_pool_index_t;
+  using ValueArray = Game::Types::ValueArray;
 
  public:
   ManagerTest():
@@ -80,9 +82,9 @@ class ManagerTest : public testing::Test {
       return get_search_thread()->raw_history_;
     }
 
-    node_pool_index_t add_child_by_action(Node* node, action_t action) {
-
-      edge_t* edge = node->get_edge(node->get_first_edge_index() + action);
+    node_pool_index_t add_child_by_action(node_pool_index_t node_ix, action_t action) {
+      Node* node = get_node_by_index(node_ix);
+      edge_t* edge = node->get_edge(action);
       if (edge->action == action && edge->child_index != -1) {
         throw std::invalid_argument("Child already exists");
       }
@@ -99,11 +101,80 @@ class ManagerTest : public testing::Test {
       return edge->child_index;
     }
 
+    void modify_node_Q(node_pool_index_t index, ValueArray Q) {
+      Node* node = get_node_by_index(index);
+      node->stats().Q = Q;
+      node->stats().Q_sq = Q.cwiseProduct(Q);
+    }
+
+    void modify_node_RN(node_pool_index_t index, int RN) {
+      Node* node = get_node_by_index(index);
+      node->stats().RN = RN;
+    }
+
+    std::string print_tree(node_pool_index_t node_ix, int num_indent=0) {
+      std::ostringstream oss;
+      Node* node = get_node_by_index(node_ix);
+
+      if (num_indent == 0) {
+        oss << std::string(num_indent * 2, ' ');
+      }
+      else{
+        oss << std::string((num_indent - 1) * 2, ' ') << "|-";
+      }
+      oss << "Node " << node_ix << ": RN = " << node->stats().RN
+      << ": Q= " << node->stats().Q.transpose() << std::endl;
+
+      for (int i = 0; i < node->stable_data().num_valid_actions; ++i) {
+        edge_t* edge = node->get_edge(i);
+
+        if (edge->child_index == -1) {
+          continue;
+        }
+        edge_pool_index_t edge_index = i + node->get_first_edge_index();
+
+        oss << std::string(num_indent * 2, ' ') << "|-"
+        << "Edge " << edge_index
+        << ": " << " E = " << edge->N << ", Action = " << edge->action << std::endl;
+        if (edge->child_index != -1) {
+          oss << print_tree(edge->child_index, num_indent + 2);
+        }
+      }
+      return oss.str();
+    }
 
  private:
   ManagerParams manager_params_;
   Manager manager_;
 };
+
+TEST_F(ManagerTest, construct_tree) {
+  start_manager();
+  start_threads();
+  search(10);
+  EXPECT_EQ(print_tree(0),
+  "Node 0: RN = 11: Q= 0.5 0.5\n"
+  "|-Edge 0:  E = 4, Action = 0\n"
+  "  |-Node 1: RN = 4: Q= 0.5 0.5\n"
+  "    |-Edge 3:  E = 1, Action = 0\n"
+  "      |-Node 4: RN = 1: Q= 0.5 0.5\n"
+  "    |-Edge 4:  E = 1, Action = 1\n"
+  "      |-Node 5: RN = 1: Q= 0.5 0.5\n"
+  "    |-Edge 5:  E = 1, Action = 2\n"
+  "      |-Node 6: RN = 1: Q= 0.5 0.5\n"
+  "|-Edge 1:  E = 3, Action = 1\n"
+  "  |-Node 2: RN = 3: Q= 0.5 0.5\n"
+  "    |-Edge 6:  E = 1, Action = 0\n"
+  "      |-Node 5: RN = 1: Q= 0.5 0.5\n"
+  "    |-Edge 7:  E = 1, Action = 1\n"
+  "      |-Node 6: RN = 1: Q= 0.5 0.5\n"
+  "|-Edge 2:  E = 3, Action = 2\n"
+  "  |-Node 3: RN = 3: Q= 0.5 0.5\n"
+  "    |-Edge 9:  E = 1, Action = 0\n"
+  "      |-Node 6: RN = 1: Q= 0.5 0.5\n"
+  "    |-Edge 10:  E = 1, Action = 1\n"
+  "      |-Node 7: RN = 1: Q= 0.5 0.5\n");
+}
 
 TEST_F(ManagerTest, backprop) {
   start_manager();
@@ -111,9 +182,25 @@ TEST_F(ManagerTest, backprop) {
   search(0);
 
   init_search_thread();
-  node_pool_index_t child_index1 = add_child_by_action(get_node_by_index(0), 1);
-  node_pool_index_t child_index2 = add_child_by_action(get_node_by_index(0), 2);
-  node_pool_index_t child_index3 = add_child_by_action(get_node_by_index(1), 0);
+  node_pool_index_t child_index1 = add_child_by_action(0, 1);
+  node_pool_index_t child_index2 = add_child_by_action(child_index1, 2);
+  node_pool_index_t child_index3 = add_child_by_action(child_index2, 0);
+  ValueArray v;
+  v << 0.8, 0.2;
+  modify_node_Q(child_index2, v);
+  modify_node_RN(child_index2, 100);
+  v << 0.1, 0.9;
+  modify_node_Q(child_index3, v);
+  modify_node_RN(child_index3, 9999);
+
+  EXPECT_EQ(print_tree(0),
+  "Node 0: RN = 1: Q= 0.5 0.5\n"
+  "|-Edge 1:  E = 0, Action = 1\n"
+  "  |-Node 1: RN = 0: Q= 0.5 0.5\n"
+  "    |-Edge 5:  E = 0, Action = 2\n"
+  "      |-Node 2: RN = 100: Q= 0.8 0.2\n"
+  "        |-Edge 6:  E = 0, Action = 0\n"
+  "          |-Node 3: RN = 9999: Q= 0.1 0.9\n");
 }
 
 int main(int argc, char** argv) {
