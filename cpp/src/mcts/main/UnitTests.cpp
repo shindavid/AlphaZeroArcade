89471e22905202116f5e61dfd0d1fce1e939d295
Lichen Song <lichen.song.personal@gmail.com>
2024-10-22 23:12:38 -0500

    add node function

--- cpp/src/mcts/main/UnitTests.cpp
+++ cpp/src/mcts/main/UnitTests.cpp
@@ -24,6 +24,11 @@ class ManagerTest : public testing::Test {
   using ManagerParams = mcts::ManagerParams<Game>;
   using Node = mcts::Node<Game>;
   using StateHistory = Game::StateHistory;
+  using SearchThread = mcts::SearchThread<Game>;
+  using action_t = core::action_t;
+  using edge_t = mcts::Node<Game>::edge_t;
+  using LookupTable = mcts::Node<Game>::LookupTable;
+  using node_pool_index_t = mcts::Node<Game>::node_pool_index_t;
 
  public:
   ManagerTest():
@@ -53,6 +58,48 @@ class ManagerTest : public testing::Test {
       return manager_.shared_data_.get_root_node();
     }
 
+    Node* get_node_by_index(node_pool_index_t index) {
+      return manager_.shared_data_.lookup_table.get_node(index);
+    }
+
+    SearchThread* get_search_thread() {
+      return manager_.search_threads_[0];
+    }
+
+    void init_search_thread() {
+      SearchThread* search_thread = get_search_thread();
+      search_thread->search_path_.clear();
+      search_thread->search_path_.emplace_back(get_root_node());
+    }
+
+    LookupTable& lookup_table() {
+      return manager_.shared_data_.lookup_table;
+    }
+
+    StateHistory& get_raw_history() {
+      return get_search_thread()->raw_history_;
+    }
+
+    node_pool_index_t add_child_by_action(Node* node, action_t action) {
+
+      edge_t* edge = node->get_edge(node->get_first_edge_index() + action);
+      if (edge->action == action && edge->child_index != -1) {
+        throw std::invalid_argument("Child already exists");
+      }
+
+      Game::Rules::apply(get_raw_history(), edge->action);
+
+      edge->child_index = lookup_table().alloc_node();
+      Node* child = lookup_table().get_node(edge->child_index);
+      new (child) Node(&lookup_table(), get_raw_history());
+
+      child->initialize_edges();
+      get_search_thread()->init_node(&get_raw_history(), edge->child_index, child);
+
+      return edge->child_index;
+    }
+
+
  private:
   ManagerParams manager_params_;
   Manager manager_;
@@ -63,9 +110,10 @@ TEST_F(ManagerTest, backprop) {
   start_threads();
   search(0);
 
-  Node* root = get_root_node();
-
-  std::cout << "root N: " << root->stats().RN << std::endl;
+  init_search_thread();
+  node_pool_index_t child_index1 = add_child_by_action(get_node_by_index(0), 1);
+  node_pool_index_t child_index2 = add_child_by_action(get_node_by_index(0), 2);
+  node_pool_index_t child_index3 = add_child_by_action(get_node_by_index(1), 0);
 }
 
 int main(int argc, char** argv) {
