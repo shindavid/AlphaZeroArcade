12373304ac84d331856c49053b4c4eaf334af895
Lichen Song <lichen.song.personal@gmail.com>
2024-10-28 17:50:28 -0500

    add IOBase.hpp

---
+++ cpp/include/core/IOBase.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace core {
+
+template <typename Types, typename State, typename MCTSKey>
+struct IOBase {
+    static std::string action_delimiter() { return "-"; }
+    static std::string action_to_str(core::action_t action) { return std::to_string(action); }
+    static void print_state(std::ostream&, const State&, core::action_t last_action = -1,
+                            const Types::player_name_array_t* player_names = nullptr) {
+      throw std::runtime_error("Not implemented");
+    }
+    static void print_mcts_results(std::ostream&, const Types::PolicyTensor& action_policy,
+                                   const Types::SearchResults&) {
+      throw std::runtime_error("Not implemented");
+    }
+    static std::string compact_state_repr(const MCTSKey& key) {
+      throw std::runtime_error("Not implemented");
+    }
+};
+
+}  // namespace core
