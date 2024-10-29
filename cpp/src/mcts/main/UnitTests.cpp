4de1e4e265e8ea94e1b45da5361ce91e70595e7a
David Shin <shin.david@gmail.com>
2024-10-29 16:56:21 -0500

    indentation fix

--- cpp/src/mcts/main/UnitTests.cpp
+++ cpp/src/mcts/main/UnitTests.cpp
@@ -100,91 +100,91 @@ class ManagerTest : public testing::Test {
   //   delete manager_;
   // }
 
-    static ManagerParams create_manager_params() {
-      ManagerParams params(mcts::kCompetitive);
-      params.no_model = true;
-      return params;
-    }
+  static ManagerParams create_manager_params() {
+    ManagerParams params(mcts::kCompetitive);
+    params.no_model = true;
+    return params;
+  }
 
-    void init_manager(Service* service=nullptr) {
-      manager_ = new Manager(manager_params_, service);
-    }
+  void init_manager(Service* service=nullptr) {
+    manager_ = new Manager(manager_params_, service);
+  }
 
-    void start_manager(const std::vector<core::action_t>& initial_actions) {
-      manager_->start();
-      for (core::action_t action : initial_actions) {
-        manager_->shared_data_.update_state(action);
+  void start_manager(const std::vector<core::action_t>& initial_actions) {
+    manager_->start();
+    for (core::action_t action : initial_actions) {
+      manager_->shared_data_.update_state(action);
+    }
+    this->initial_actions_ = initial_actions;
+  }
+
+  void start_threads() {
+    manager_->start_threads();
+  }
+
+  void search(int num_searches = 0){
+    mcts::SearchParams search_params(num_searches, true);
+    manager_->search(search_params);
+  }
+
+  Node* get_node_by_index(node_pool_index_t index) {
+    return manager_->shared_data_.lookup_table.get_node(index);
+  }
+
+  std::string print_tree(node_pool_index_t node_ix, const State& prev_state, int num_indent=0) {
+    std::ostringstream oss;
+    Node* node = get_node_by_index(node_ix);
+
+    if (num_indent == 0) {
+      oss << std::string(num_indent * 2, ' ');
+    } else {
+      const char* marker = node->is_terminal() ? "|*" : "|-";
+      oss << std::string((num_indent - 1) * 2, ' ') << marker;
+    }
+    oss << "Node " << node_ix << ": " << Game::IO::state_repr(prev_state)
+        << " RN = " << node->stats().RN << ": Q = " << node->stats().Q.transpose() << std::endl;
+
+    for (int i = 0; i < node->stable_data().num_valid_actions; ++i) {
+      edge_t* edge = node->get_edge(i);
+
+      if (edge->child_index == -1) {
+        continue;
       }
-      this->initial_actions_ = initial_actions;
-    }
+      edge_pool_index_t edge_index = i + node->get_first_edge_index();
 
-    void start_threads() {
-      manager_->start_threads();
-    }
-
-    void search(int num_searches = 0){
-      mcts::SearchParams search_params(num_searches, true);
-      manager_->search(search_params);
-    }
-
-    Node* get_node_by_index(node_pool_index_t index) {
-      return manager_->shared_data_.lookup_table.get_node(index);
-    }
-
-    std::string print_tree(node_pool_index_t node_ix, const State& prev_state, int num_indent=0) {
-      std::ostringstream oss;
-      Node* node = get_node_by_index(node_ix);
-
-      if (num_indent == 0) {
-        oss << std::string(num_indent * 2, ' ');
-      } else {
-        const char* marker = node->is_terminal() ? "|*" : "|-";
-        oss << std::string((num_indent - 1) * 2, ' ') << marker;
-      }
-      oss << "Node " << node_ix << ": " << Game::IO::state_repr(prev_state)
-          << " RN = " << node->stats().RN << ": Q = " << node->stats().Q.transpose() << std::endl;
-
-      for (int i = 0; i < node->stable_data().num_valid_actions; ++i) {
-        edge_t* edge = node->get_edge(i);
-
-        if (edge->child_index == -1) {
-          continue;
-        }
-        edge_pool_index_t edge_index = i + node->get_first_edge_index();
-
-        State new_state = prev_state;
-        StateHistory history;
-        history.update(new_state);
-        Game::Rules::apply(history, edge->action);
-        new_state = history.current();
-
-        oss << std::string(num_indent * 2, ' ') << "|-"
-        << "Edge " << edge_index
-        << ": " << " E = " << edge->E << ", Action = " << edge->action << std::endl;
-        if (edge->child_index != -1) {
-          oss << print_tree(edge->child_index, new_state, num_indent + 2);
-        }
-      }
-      return oss.str();
-    }
-
-    /*
-     * This function prints the tree structure starting from root.
-     * It recursively traverses the tree and prints each node and its edges.
-     * The format is as follows:
-     * - Each node is printed with its index, state representation, RN (visit count), and Q values.
-     * - Each edge is printed with its index, visit count (E), and action.
-     * - Indentation is used to represent the tree structure, with each level of depth indented
-     * further.
-     */
-    std::string print_tree() {
+      State new_state = prev_state;
       StateHistory history;
-      history.initialize(Game::Rules{});
-      for (core::action_t action : initial_actions_) {
-        Game::Rules::apply(history, action);
+      history.update(new_state);
+      Game::Rules::apply(history, edge->action);
+      new_state = history.current();
+
+      oss << std::string(num_indent * 2, ' ') << "|-"
+      << "Edge " << edge_index
+      << ": " << " E = " << edge->E << ", Action = " << edge->action << std::endl;
+      if (edge->child_index != -1) {
+        oss << print_tree(edge->child_index, new_state, num_indent + 2);
       }
-      return print_tree(0, history.current());
     }
+    return oss.str();
+  }
+
+  /*
+   * This function prints the tree structure starting from root.
+   * It recursively traverses the tree and prints each node and its edges.
+   * The format is as follows:
+   * - Each node is printed with its index, state representation, RN (visit count), and Q values.
+   * - Each edge is printed with its index, visit count (E), and action.
+   * - Indentation is used to represent the tree structure, with each level of depth indented
+   * further.
+   */
+  std::string print_tree() {
+    StateHistory history;
+    history.initialize(Game::Rules{});
+    for (core::action_t action : initial_actions_) {
+      Game::Rules::apply(history, action);
+    }
+    return print_tree(0, history.current());
+  }
 
  private:
   ManagerParams manager_params_;
