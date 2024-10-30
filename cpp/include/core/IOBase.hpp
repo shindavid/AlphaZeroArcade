da5bc582c8c15f11cb075c15e5c8d8671412d6e4
Lichen Song <lichen.song.personal@gmail.com>
2024-10-30 15:38:03 -0500

    fix Not to not

--- cpp/include/core/IOBase.hpp
+++ cpp/include/core/IOBase.hpp
@@ -8,7 +8,7 @@ struct IOBase {
     static std::string action_to_str(core::action_t action) { return std::to_string(action); }
     static void print_state(std::ostream&, const State&, core::action_t last_action = -1,
                             const Types::player_name_array_t* player_names = nullptr) {
-      throw std::runtime_error("print_state Not implemented");
+      throw std::runtime_error("print_state not implemented");
     }
     static void print_mcts_results(std::ostream&, const Types::PolicyTensor& action_policy,
                                    const Types::SearchResults&) {
