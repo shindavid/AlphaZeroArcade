2e3069d8e360f5b9857f56f65a1300b6201fa2c6
Lichen Song <lichen.song.personal@gmail.com>
2024-11-08 10:23:39 -0600

    produce action_response and goldenfiles

--- cpp/src/generic_players/main/UnitTests.cpp
+++ cpp/src/generic_players/main/UnitTests.cpp
@@ -6,8 +6,12 @@
 #include <mcts/Manager.hpp>
 #include <mcts/ManagerParams.hpp>
 #include <mcts/SearchLog.hpp>
+#include <util/EigenUtil.hpp>
+#include <util/BoostUtil.hpp>
 
 #include <gtest/gtest.h>
+
+#include <fstream>
 
 template<core::concepts::Game Game>
 class MctsPlayerTest : public ::testing::Test {
@@ -23,7 +27,8 @@ class MctsPlayerTest : public ::testing::Test {
   using StateHistory = Game::StateHistory;
   using State = Game::State;
   using ActionMask = Game::Types::ActionMask;
-    using Service = mcts::NNEvaluationServiceBase<Game>;
+  using Service = mcts::NNEvaluationServiceBase<Game>;
+  using Rules = Game::Rules;
 
  public:
   MctsPlayerTest() : manager_params_(create_manager_params()), player_params_(mcts::kCompetitive) {}
@@ -34,19 +39,24 @@ class MctsPlayerTest : public ::testing::Test {
     return params;
   }
 
-  void init_manager(Service* service = nullptr) {
+  void init(Service* service = nullptr) {
     mcts_manager_ = new Manager(manager_params_, service);
-    const mcts::SharedData<Game>* shared_data = manager_->shared_data();
+    const mcts::SharedData<Game>* shared_data = mcts_manager_->shared_data();
     search_log_ = new mcts::SearchLog<Game>(shared_data);
-    manager_->set_post_visit_func([&] { search_log_->update(); });
+    mcts_manager_->set_post_visit_func([&] { search_log_->update(); });
+    mcts_manager_->start();
+    mcts_manager_->set_player_data(mcts_manager_->shared_data());
+    mcts_player_ = new MctsPlayer(player_params_, mcts_manager_);
   }
 
   void start_manager(const std::vector<core::action_t>& initial_actions = {}) {
-    manager_->start();
+    mcts_player_->start_game();
+    mcts_manager_->start_threads();
     for (core::action_t action : initial_actions) {
-      manager_->shared_data()->update_state(action);
+      auto shared_data = mcts_manager_->shared_data();
+      shared_data->update_state(action);
     }
-    this->initial_actions_ = initial_actions;
+    initial_actions_ = initial_actions;
   }
 
   core::ActionResponse get_action_response(SearchResult* search_result,
@@ -60,7 +70,10 @@ class MctsPlayerTest : public ::testing::Test {
     return response;
   }
 
+  mcts::SearchLog<Game>* get_search_log() { return search_log_; }
+
   void TearDown() override {
+    delete search_log_;
     delete mcts_manager_;
     delete mcts_player_;
   }
@@ -76,12 +89,47 @@ private:
 
 using TicTacToeMctsPlayerTest = MctsPlayerTest<tictactoe::Game>;
 TEST_F(TicTacToeMctsPlayerTest, uniform_search_01247) {
-  init_manager();
+  init();
   std::vector<core::action_t> initial_actions = {0, 1, 2, 4, 7};
   start_manager(initial_actions);
   SearchResult search_result;
   PolicyTensor output_policy;
   core::ActionResponse response = get_action_response(&search_result, &output_policy);
+
+  std::stringstream ss_result, ss_policy;
+  boost_util::pretty_print(ss_result, search_result.to_json());
+  boost_util::pretty_print(ss_policy, eigen_util::to_json(output_policy));
+
+  boost::filesystem::path file_path_result =
+      util::Repo::root() / "goldenfiles" / "generic_players" / "tictactoe01247_result.json";
+
+  boost::filesystem::path file_path_policy =
+      util::Repo::root() / "goldenfiles" / "generic_players" / "tictactoe01247_policy.json";
+
+  boost::filesystem::path file_path_log =
+      util::Repo::root() / "goldenfiles" / "generic_players" / "tictactoe01247_log.json";
+
+  if (IS_MACRO_ENABLED(WRITE_GOLDENFILES)) {
+    boost_util::write_str_to_file(ss_result.str(), file_path_result);
+    boost_util::write_str_to_file(ss_policy.str(), file_path_policy);
+    boost_util::write_str_to_file(get_search_log()->json_str(), file_path_log);
+  }
+
+  std::ifstream result_file(file_path_result);
+  std::ifstream policy_file(file_path_policy);
+  std::ifstream log_file(file_path_log);
+
+  std::string expected_result_json((std::istreambuf_iterator<char>(result_file)),
+                                   std::istreambuf_iterator<char>());
+  std::string expected_policy_json((std::istreambuf_iterator<char>(policy_file)),
+                                    std::istreambuf_iterator<char>());
+  std::string expected_log_json((std::istreambuf_iterator<char>(log_file)),
+                                std::istreambuf_iterator<char>());
+
+  EXPECT_EQ(ss_result.str(), expected_result_json);
+  EXPECT_EQ(ss_policy.str(), expected_policy_json);
+  EXPECT_EQ(get_search_log()->json_str(), expected_log_json);
+  EXPECT_EQ(response.action, 6);
 }
 
 int main(int argc, char** argv) {
