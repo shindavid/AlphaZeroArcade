2b338ac59040731e4084d4605a4a7a1f91003027
Lichen Song <lichen.song.personal@gmail.com>
2024-12-20 20:17:48 -0600

    delete unused function

--- cpp/include/games/nim/Game.hpp
+++ cpp/include/games/nim/Game.hpp
@@ -70,7 +70,6 @@ struct Game {
     static void apply(StateHistory& history, core::action_t action);
     static bool is_terminal(const State& state, core::seat_index_t last_player,
                             core::action_t last_action, GameResults::Tensor& outcome);
-    static int get_num_chance_actions() { return kMaxRandomStonesToTake + 1; }
     static bool has_known_dist(const State& state) { return (get_action_mode(state) == 1); }
     static Types::PolicyTensor get_known_dist(const State& state);
   };
