21f68dd96809dda2cbe0eebc3fc3c27cab3034c1
David Shin <shin.david@gmail.com>
2024-12-30 13:05:51 +0900

    require explicitly specified optimizer in each game-spec

--- py/games/connect4/spec.py
+++ py/games/connect4/spec.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import math
 
 from games.game_spec import GameSpec, ReferencePlayerFamily
-from shared.net_modules import ModelConfig, ModuleSpec, ShapeInfoDict
+from shared.net_modules import ModelConfig, ModuleSpec, OptimizerSpec, ShapeInfoDict
 from shared.training_params import TrainingParams
 
 
@@ -61,6 +61,8 @@ def b7_c128(shape_info_dict: ShapeInfoDict):
             'action_value': 1.0,
             'opp_policy': 0.15,
         },
+
+        opt=OptimizerSpec(type='SGD', kwargs={'lr': 6e-5, 'weight_decay': 6e-5, 'momentum': 0.9}),
     )
 
 
@@ -113,6 +115,8 @@ def transformer(shape_info_dict: ShapeInfoDict):
             'action_value': 1.0,
             'opp_policy': 0.15,
         },
+
+        opt=OptimizerSpec(type='RAdam', kwargs={'lr': 6e-5, 'weight_decay': 6e-5}),
     )
 
 
