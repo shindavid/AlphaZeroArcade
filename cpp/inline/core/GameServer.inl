shindavid/AlphaZeroArcade

commit 1f5cba5c91013f17e2c8b57c0873874619980308

move raw-policy mechanics from MCTSPlayer to GameServer

cpp/inline/core/GameServer.inl
@@ -37,6 +37,9 @@ auto GameServer<Game>::Params::make_options_description() {
           po::value<int>(&parallelism)->default_value(parallelism),
           "max num games to play simultaneously. Participating players can request lower values "
           "and the server will respect their requests")
+      .template add_option<"mean-noisy-moves", 'n'>(
+          po2::float_value("%.2f", &mean_noisy_moves, mean_noisy_moves),
+          "mean number of noisy moves to make at the start of each game")
       .template add_flag<"display-progress-bar", "hide-progress-bar">(
           &display_progress_bar, "display progress bar (only in tty-mode without TUI player)",
           "hide progress bar")
@@ -280,6 +283,7 @@ void GameServer<Game>::GameThread::run() {
 template <concepts::Game Game>
 typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
     player_array_t& players) {
+  const Params& params = shared_data_.params();
   game_id_t game_id = util::get_unique_id();
 
   TrainingDataWriter* training_data_writer = shared_data_.training_data_writer();
@@ -295,14 +299,20 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
     players[p]->start_game();
   }
 
+  int num_noisy_starting_moves = 0;
+  if (params.mean_noisy_moves) {
+    num_noisy_starting_moves = util::Random::exponential(1.0 / params.mean_noisy_moves);
+  }
+  int move_number = 0;
+  bool noisy_mode = move_number < num_noisy_starting_moves;
+
   StateHistory state_history;
   state_history.initialize(Rules{});
   seat_index_t active_seat = -1;
 
-  if (shared_data_.params().print_game_states) {
+  if (params.print_game_states) {
     Game::IO::print_state(std::cout, state_history.current(), -1, &player_names);
   }
-
 
   while (true) {
     core::action_mode_t action_mode = Rules::get_action_mode(state_history.current());
@@ -314,11 +324,12 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
       ChanceDistribution chance_dist = Rules::get_chance_distribution(state_history.current());
       action = eigen_util::sample(chance_dist);
       if (game_log) {
+        // TODO: AV-target should be nullptr iff noisy_mode is true
         game_log->add(state_history.current(), action, active_seat, nullptr, nullptr, true);
       }
 
       Rules::apply(state_history, action);
-      if (shared_data_.params().print_game_states) {
+      if (params.print_game_states) {
         Game::IO::print_state(std::cout, state_history.current(), action, &player_names);
       }
       for (auto player2 : players) {
@@ -327,10 +338,12 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
 
       terminal = Game::Rules::is_terminal(state_history.current(), active_seat, action, outcome);
     } else {
+      noisy_mode = move_number < num_noisy_starting_moves;
+
       active_seat = Rules::get_current_player(state_history.current());
       Player* player = players[active_seat];
       auto valid_actions = Rules::get_legal_moves(state_history);
-      ActionRequest request(state_history.current(), valid_actions);
+      ActionRequest request(state_history.current(), valid_actions, noisy_mode);
       ActionResponse response = player->get_action_response(request);
       action = response.action;
       const TrainingInfo& training_info = response.training_info;
@@ -343,17 +356,17 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
       // the server.
       util::release_assert(valid_actions[action], "Invalid action: %d", action);
 
-      if (response.victory_guarantee && shared_data_.params().respect_victory_hints) {
+      if (response.victory_guarantee && params.respect_victory_hints) {
         outcome = GameResults::win(active_seat);
         terminal = true;
-        if (shared_data_.params().announce_game_results) {
+        if (params.announce_game_results) {
           printf("Short-circuiting game %ld because player %s (seat=%d) claims victory\n", game_id,
                 player->get_name().c_str(), int(active_seat));
           std::cout << std::endl;
         }
       } else {
         Rules::apply(state_history, action);
-        if (shared_data_.params().print_game_states) {
+        if (params.print_game_states) {
           Game::IO::print_state(std::cout, state_history.current(), action, &player_names);
         }
         for (auto player2 : players) {
@@ -375,7 +388,7 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
         training_data_writer->add(game_log);
       }
 
-      if (shared_data_.params().announce_game_results) {
+      if (params.announce_game_results) {
         printf("Game %ld complete.\n", game_id);
         for (player_id_t p = 0; p < kNumPlayers; ++p) {
           printf("  pid=%d name=%s %g\n", p, players[p]->get_name().c_str(), array[p]);

cpp/include/core/GameServer.hpp
@@ -84,6 +84,7 @@ class GameServer {
     int num_games = 1000;   // if <=0, run indefinitely
     int parallelism = 256;  // number of games to run simultaneously
     int port = 0;
+    float mean_noisy_moves = 0.0;  // mean of exp distr from which to draw number of noisy moves
     bool display_progress_bar = false;
     bool print_game_states = false;  // print game state between moves
     bool announce_game_results = false;  // print outcome of each individual match

cpp/include/core/GameTypes.hpp
@@ -57,8 +57,8 @@ struct GameTypes {
     const State& state;
     const ActionMask& valid_actions;
 
-    // If set to true, the player is being asked to play noisily, in order to add diversity to the
-    // training data. Each player is free to interpret this in their own way.
+    // If set to true, the player is being asked to play noisily, in order to add opening diversity.
+    // Each player is free to interpret this in their own way.
     bool play_noisily;
   };
 

cpp/include/generic_players/MctsPlayer.hpp
@@ -35,7 +35,6 @@ class MctsPlayer : public core::AbstractPlayer<Game> {
     int num_fast_iters;
     int num_full_iters;
     float full_pct;
-    float mean_raw_moves = 0.0;
     float starting_move_temperature;
     float ending_move_temperature = 0.2;
     float move_temperature_half_life = 0.5 * Game::MctsConfiguration::kOpeningLength;
@@ -83,7 +82,7 @@ class MctsPlayer : public core::AbstractPlayer<Game> {
  protected:
   auto get_action_policy(core::SearchMode, const SearchResults*, const ActionMask&) const;
   const SearchResults* mcts_search(core::SearchMode search_mode) const;
-  core::SearchMode choose_search_mode() const;
+  core::SearchMode choose_search_mode(const ActionRequest& request) const;
   ActionResponse get_action_response_helper(core::SearchMode, const SearchResults*,
                                             const ActionMask& valid_actions) const;
 
@@ -105,7 +104,6 @@ class MctsPlayer : public core::AbstractPlayer<Game> {
   VerboseInfo* verbose_info_ = nullptr;
   const bool owns_shared_data_;
   bool facing_human_tui_player_ = false;
-  int move_count_ = 0;
 
   template<core::concepts::Game> friend class MctsPlayerTest;
 };

cpp/inline/generic_players/DataExportingMctsPlayer.inl
@@ -11,7 +11,7 @@ typename DataExportingMctsPlayer<Game>::ActionResponse
 DataExportingMctsPlayer<Game>::get_action_response(const ActionRequest& request) {
   const ActionMask& valid_actions = request.valid_actions;
 
-  auto search_mode = this->choose_search_mode();
+  auto search_mode = this->choose_search_mode(request);
 
   GameLogWriter_sptr game_log = this->get_game_log();
   bool use_for_training = game_log && search_mode == core::kFull;

cpp/inline/generic_players/MctsPlayer.inl
@@ -61,9 +61,6 @@ auto MctsPlayer<Game>::Params::make_options_description() {
           "num mcts iterations to do per full move")
       .template add_option<"full-pct", 'f'>(po2::float_value("%.2f", &full_pct, full_pct),
                                             "pct of moves that should be full")
-      .template add_option<"mean-raw-moves", 'r'>(
-          po2::float_value("%.2f", &mean_raw_moves, mean_raw_moves),
-          "mean number of raw policy moves to make at the start of each game")
       .template add_hidden_option<"starting-move-temp">(
           po::value<float>(&starting_move_temperature)->default_value(starting_move_temperature),
           "starting temperature for move selection")
@@ -113,21 +110,15 @@ inline MctsPlayer<Game>::~MctsPlayer() {
 
 template <core::concepts::Game Game>
 inline void MctsPlayer<Game>::start_game() {
-  move_count_ = 0;
   move_temperature_.reset();
   if (owns_shared_data_) {
     get_manager()->start();
-    if (params_.mean_raw_moves) {
-      shared_data_->num_raw_policy_starting_moves =
-          util::Random::exponential(1.0 / params_.mean_raw_moves);
-    }
   }
 }
 
 template <core::concepts::Game Game>
 inline void MctsPlayer<Game>::receive_state_change(core::seat_index_t seat, const State& state,
                                                     core::action_t action) {
-  move_count_++;
   move_temperature_.step();
   if (owns_shared_data_) {
     get_manager()->receive_state_change(seat, state, action);
@@ -146,7 +137,7 @@ inline void MctsPlayer<Game>::receive_state_change(core::seat_index_t seat, cons
 template <core::concepts::Game Game>
 typename MctsPlayer<Game>::ActionResponse
 MctsPlayer<Game>::get_action_response(const ActionRequest& request) {
-  core::SearchMode search_mode = choose_search_mode();
+  core::SearchMode search_mode = choose_search_mode(request);
   const SearchResults* mcts_results = mcts_search(search_mode);
   return get_action_response_helper(search_mode, mcts_results, request.valid_actions);
 }
@@ -158,9 +149,8 @@ inline const typename MctsPlayer<Game>::SearchResults* MctsPlayer<Game>::mcts_se
 }
 
 template <core::concepts::Game Game>
-inline core::SearchMode MctsPlayer<Game>::choose_search_mode() const {
-  bool use_raw_policy = move_count_ < shared_data_->num_raw_policy_starting_moves;
-  return use_raw_policy ? core::kRawPolicy : get_random_search_mode();
+inline core::SearchMode MctsPlayer<Game>::choose_search_mode(const ActionRequest& request) const {
+  return request.play_noisily ? core::kRawPolicy : get_random_search_mode();
 }
 
 template <core::concepts::Game Game>

cpp/src/generic_players/main/UnitTests.cpp
@@ -31,6 +31,7 @@ class MctsPlayerTest : public ::testing::Test {
   using PolicyTensor = Game::Types::PolicyTensor;
   using StateHistory = Game::StateHistory;
   using State = Game::State;
+  using ActionRequest = Game::Types::ActionRequest;
   using ActionMask = Game::Types::ActionMask;
   using Service = mcts::NNEvaluationServiceBase<Game>;
   using Rules = Game::Rules;
@@ -79,7 +80,8 @@ class MctsPlayerTest : public ::testing::Test {
         mcts_player_->get_manager()->shared_data()->root_info.history_array[group::kIdentity];
     ActionMask valid_actions = Rules::get_legal_moves(state_history);
 
-    core::SearchMode search_mode = mcts_player_->choose_search_mode();
+    ActionRequest request(state_history.current(), valid_actions);
+    core::SearchMode search_mode = mcts_player_->choose_search_mode(request);
     const SearchResults* search_result = mcts_player_->mcts_search(search_mode);
 
     PolicyTensor modified_policy =

py/games/blokus/spec.py
@@ -110,8 +110,8 @@ class BlokusSpec(GameSpec):
         minibatch_size=64,
     )
 
-    training_player_options = {
-        '-r': 8,
+    training_options = {
+        '--mean-noisy-moves': 8,
     }
 
 

commit 0145288fb8a68353b221347f5106b8c3efd9fa90

fix bug introduced in 1f5cba5c91013f17e2c8b57c0873874619980308

cpp/inline/core/GameServer.inl
@@ -303,7 +303,7 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
   if (params.mean_noisy_moves) {
     num_noisy_starting_moves = util::Random::exponential(1.0 / params.mean_noisy_moves);
   }
-  int move_number = 0;
+  int move_number = 0;  // tracks player-actions, not chance-events
   bool noisy_mode = move_number < num_noisy_starting_moves;
 
   StateHistory state_history;
@@ -349,6 +349,7 @@ typename GameServer<Game>::ValueArray GameServer<Game>::GameThread::play_game(
       terminal = Game::Rules::is_terminal(state_history.current(), active_seat, action, outcome);
     } else {
       noisy_mode = move_number < num_noisy_starting_moves;
+      move_number++;
 
       active_seat = Rules::get_current_player(state_history.current());
       Player* player = players[active_seat];