9ca30f0d8c7790e8ded0bcdd8d9a51f990304c61
Lichen Song <lichen.song.personal@gmail.com>
2025-02-04 22:20:17 -0600

    add_agent

--- py/alphazero/scripts/compute_benchmark_rating.py
+++ py/alphazero/scripts/compute_benchmark_rating.py
@@ -10,15 +10,11 @@ from networkx.drawing.nx_agraph import graphviz_layout
 from tqdm import tqdm
 
 from itertools import combinations
-from typing import List
+from typing import List, Dict
 import matplotlib.pyplot as plt
 import numpy as np
 from dataclasses import dataclass
 
-game = 'c4'
-tag = 'benchmark'
-binary = f'/workspace/repo/target/Release/bin/{game}'
-database = f'/workspace/repo/output/{game}/{tag}/benchmarking.db'
 logger = get_logger()
 
 @dataclass(frozen=True)
@@ -73,7 +69,7 @@ def get_mcts_player_str(gen: int, n_iters: int):
         player_args = {
             '--type': 'MCTS-C',
             '--name': name,
-            '-i': n_iters,
+            '-i': 0,
             '-n': 1,
             '--no-model': None,
         }
@@ -126,12 +122,21 @@ def run_match_helper(agent1: Agent, agent2: Agent, n_games=100):
     return record.get(0)
 
 class BenchmarkCommittee:
-    def __init__(self):
+    def __init__(self, game, tag, database_name):
       self.W_matrix = np.zeros((0, 0), dtype=float)
       self.G = nx.Graph()
+      database = self.get_database_path(game, tag, database_name)
       self.benchmarking_db_conn_pool = DatabaseConnectionPool(database, constants.BENCHMARKING_TABLE_CREATE_CMDS)
       self.ratings = None
       self.load_past_data()
+
+    @staticmethod
+    def get_database_path(game, tag, database_name):
+      return f'/workspace/repo/output/{game}/{tag}/{database_name}.db'
+
+    @staticmethod
+    def get_binary_path(game):
+      return f'/workspace/repo/target/Release/bin/{game}'
 
     def expand_matrix(self):
       n = self.W_matrix.shape[0]
@@ -178,24 +183,25 @@ class BenchmarkCommittee:
         c.execute('INSERT INTO matches VALUES (?, ?, ?, ?, ?, ?, ?, ?)', match_tuple)
         conn.commit()
 
+    def add_agent(self, agent: Agent):
+        if agent not in self.G.nodes:
+            ix = len(self.G.nodes)
+            self.G.add_node(agent, ix=ix)
+            self.expand_matrix()
+            return ix, True
+        else:
+            ix = self.G.nodes[agent]['ix']
+            return ix, False
+
     def play_matches(self, gen: int, n_iters: int):
         matches = get_matches(gen)
         for (x, y) in tqdm(matches):
             agent1 = Agent(gen=x, n_iters=n_iters)
-            if agent1 not in self.G.nodes:
-                ix1 = len(self.G.nodes)
-                self.G.add_node(agent1, ix=ix1)
-                self.expand_matrix()
-            else:
-                ix1 = self.G.nodes[agent1]['ix']
+            ix1, _ = self.add_agent(agent1)
 
             agent2 = Agent(gen=y, n_iters=n_iters)
-            if agent2 not in self.G.nodes:
-                ix2 = len(self.G.nodes)
-                self.G.add_node(agent2, ix=ix2)
-                self.expand_matrix()
-            else:
-                ix2 = self.G.nodes[agent2]['ix']
+            ix2, _ = self.add_agent(agent2)
+
             if not self.G.has_edge(agent1, agent2):
                 self.G.add_edge(agent1, agent2)
                 result = run_match_helper(agent1, agent2)
@@ -207,18 +213,38 @@ class BenchmarkCommittee:
         ratings = compute_ratings(self.W_matrix)
         self.ratings = {agent: ratings[ix] for agent, ix in self.G.nodes(data='ix')}
 
+def save_ratings_plt(agents, ratings, filename: str):
+    agent_labels = np.array(list(agents)).astype(str)
+    plt.figure()
+    plt.plot(agent_labels, ratings, label='Rating')
+    plt.xticks(agent_labels[::5])
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.show()
+    plt.savefig(filename)
+
 if __name__ == '__main__':
-    committee = BenchmarkCommittee()
-    committee.play_matches(512, 0)
+    game = 'c4'
+    tag = 'benchmark'
+    database_name = 'benchmarking'
+    committee = BenchmarkCommittee(game, tag, database_name)
+    committee.play_matches(512, 1)
     committee.play_matches(512, 100)
     committee.compute_ratings()
 
     ratings = committee.ratings
-    agent_labels = np.array(list(ratings.keys())).astype(str)
-    plt.plot(agent_labels, ratings.values(), label='Rating')
-    plt.legend()
-    plt.show()
-    plt.savefig("ratings.png")
+
+    agents_i1 = [agent for agent in committee.G.nodes if agent.n_iters == 1]
+    i1_ix = [ix for agent, ix in committee.G.nodes(data='ix') if agent.n_iters == 1]
+
+    agents_i100 = [agent for agent in committee.G.nodes if agent.n_iters == 100]
+    i100_ix = [ix for agent, ix in committee.G.nodes(data='ix') if agent.n_iters == 100]
+
+    ratings_i1 = compute_ratings(committee.W_matrix[i1_ix, :][:, i1_ix])
+    ratings_i100 = compute_ratings(committee.W_matrix[i100_ix, :][:, i100_ix])
+
+    save_ratings_plt(agents_i1, ratings_i1, 'ratings_i1.png')
+    save_ratings_plt(agents_i100, ratings_i100, 'ratings_i100.png')
 
     plt.figure()
     pos = graphviz_layout(committee.G, prog="dot")
@@ -239,7 +265,7 @@ if __name__ == '__main__':
         num_of_matches.append(len(m))
         num_of_gens.append(2**i)
         num_of_nodes.append(len(np.unique(m)))
-        print(f"{2**i}: {np.unique(m)}")
+
     plt.plot(num_of_gens, num_of_matches, label='Number of Matches')
     plt.plot(num_of_gens, num_of_nodes, label='Number of Gens')
     plt.legend()
