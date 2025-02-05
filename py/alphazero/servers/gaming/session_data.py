shindavid/AlphaZeroArcade

commit c63d5dd73869463c600e219faf97a61d45ad45f5

fix potential deadlock

py/alphazero/servers/gaming/session_data.py
@@ -6,6 +6,7 @@ from games.index import get_game_spec
 from util.logging_util import LoggingParams, configure_logger, get_logger
 from util.socket_util import Socket
 from util import ssh_util
+from util import subprocess_util
 
 import os
 import socket
@@ -37,16 +38,10 @@ class SessionData:
         self._skip_next_returncode_check = True
 
     def wait_for(self, proc: subprocess.Popen):
-        proc.wait()
-
-        if self._skip_next_returncode_check:
-            self._skip_next_returncode_check = False
-            return
-        if proc.returncode:
-            logger.error(f'Process failed with return code {proc.returncode}')
-            for line in proc.stderr:
-                logger.error(line.strip())
-            raise Exception()
+        expected_rc = None if self._skip_next_returncode_check else 0
+        print_fn = logger.error
+        self._skip_next_returncode_check = False
+        return subprocess_util.wait_for(proc, expected_return_code=expected_rc, print_fn=print_fn)
 
     def init_socket(self):
         addr = (self._params.loop_controller_host, self._params.loop_controller_port)

py/alphazero/servers/gaming/ratings_server.py
@@ -180,12 +180,12 @@ class RatingsServer:
 
         proc = subprocess_util.Popen(cmd)
         logger.info('Running %s vs %s match [%s]: %s', mcts_name, ref_name, proc.pid, cmd)
-        self._session_data.wait_for(proc)
+        stdout = self._session_data.wait_for(proc)
 
         # NOTE: extracting the match record from stdout is potentially fragile. Consider
         # changing this to have the c++ process directly communicate its win/loss data to the
         # loop-controller. Doing so would better match how the self-play server works.
-        record = extract_match_record(proc.stdout)
+        record = extract_match_record(stdout)
         logger.info('Match result: %s', record.get(0))
 
         self._running = False

py/alphazero/servers/gaming/self_play_server.py
@@ -197,7 +197,7 @@ class SelfPlayServer:
         self_play_cmd.append(make_args_str(args))
         self_play_cmd = ' '.join(map(str, self_play_cmd))
 
-        proc = subprocess_util.Popen(self_play_cmd)
+        proc = subprocess_util.Popen(self_play_cmd, stdout=subprocess.DEVNULL)
         logger.info('Running gen-0 self-play [%s]: %s', proc.pid, self_play_cmd)
         self._session_data.wait_for(proc)
 
@@ -262,7 +262,7 @@ class SelfPlayServer:
         self_play_cmd.append(make_args_str(args))
         self_play_cmd = ' '.join(map(str, self_play_cmd))
 
-        proc = subprocess_util.Popen(self_play_cmd)
+        proc = subprocess_util.Popen(self_play_cmd, stdout=subprocess.DEVNULL)
         self._proc = proc
         logger.info('Running self-play [%s]: %s', proc.pid, self_play_cmd)
         self._session_data.wait_for(proc)

py/util/subprocess_util.py
@@ -44,9 +44,6 @@ def wait_for(proc: subprocess.Popen, timeout=None, expected_return_code: Optiona
     if expected_return_code not in (proc.returncode, None):
         print_fn(f'Expected rc={expected_return_code}, got rc={proc.returncode}')
         print_fn('----------------------------')
-        print_fn('STDOUT:')
-        print_fn(stdout)
-        print_fn('----------------------------')
         print_fn('STDERR:')
         print_fn(stderr)
         print_fn('----------------------------')

commit 69c458d10aaa9b36f627da8274d16c5deb7bdb38

fix hourly-restart bug introduced in c63d5dd

py/alphazero/servers/gaming/session_data.py
@@ -43,10 +43,15 @@ class SessionData:
         self._skip_next_returncode_check = True
 
     def wait_for(self, proc: subprocess.Popen):
-        expected_rc = None if self._skip_next_returncode_check else 0
-        print_fn = logger.error
-        self._skip_next_returncode_check = False
-        return subprocess_util.wait_for(proc, expected_return_code=expected_rc, print_fn=print_fn)
+        stdout, stderr = proc.communicate()
+        if self._skip_next_returncode_check:
+            self._skip_next_returncode_check = False
+        elif proc.returncode:
+            logger.error(f'Process failed with return code {proc.returncode}')
+            for line in stderr:
+                logger.error(line.strip())
+            raise Exception()
+        return stdout
 
     def init_socket(self):
         addr = (self._params.loop_controller_host, self._params.loop_controller_port)