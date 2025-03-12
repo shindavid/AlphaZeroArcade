ff541dd1b624266eb57193122c1febcdaa08d762
Lichen Song <lichen.song.personal@gmail.com>
2025-03-11 20:31:08 -0500

    rm unnecessary mutex

--- py/alphazero/servers/loop_control/eval_manager.py
+++ py/alphazero/servers/loop_control/eval_manager.py
@@ -38,7 +38,6 @@ class MatchStatus:
     opponent_gen: Generation
     n_games: int
     status: MatchRequestStatus
-    estimated_rating: Optional[float] = None
 
 
 @dataclass
@@ -87,8 +86,8 @@ class EvalManager:
 
         conn.aux['status_cond'] = threading.Condition()
         conn.aux['status'] = ServerStatus.BLOCKED
-        conn.aux['needs_new_opponents_cond'] = threading.Condition()
         conn.aux['needs_new_opponents'] = True
+        conn.aux['estimated_rating'] = None
 
         self._start()
         logger.info('Starting eval-recv-loop for %s...', conn)
@@ -140,7 +139,6 @@ class EvalManager:
     def _start(self):
         with self._lock:
             if self._started:
-                logger.info('****!!!!!!!!**** EvalManager already started ****!!!!!!!!****')
                 return
             self._started = True
             self._load_past_data()
@@ -154,6 +152,7 @@ class EvalManager:
         self._set_priority()
 
     def _handle_server_disconnect(self, conn: ClientConnection):
+        logger.info('Server disconnected: %s, evaluating gen %s', conn, conn.aux.get('gen', None))
         gen = conn.aux.pop('gen', None)
         if gen is not None:
             with self._lock:
@@ -200,64 +199,40 @@ class EvalManager:
                 lambda: len(self._eval_status_dict) < self._controller.latest_gen())
 
     def _send_match_request(self, conn: ClientConnection):
+        #TODO: remove this assert and implement mechasnim to request for binary, extra depencies or model files
         assert conn.is_on_localhost()
         gen = conn.aux.get('gen', None)
-        evaluated_gens = [data.mcts_gen for data in self._eval_status_dict.values()]
-        ratings = self._evaluator.arena_ratings[list(self._eval_status_dict.keys())]
-
         if gen is None:
-            latest_gen = self._controller.latest_gen()
-            target_rating_rate = self._controller.params.target_rating_rate
+            gen = self._get_next_gen_to_eval()
+            assert gen is not None
+            conn.aux['gen'] = gen
+            test_agent = MCTSAgent(gen, n_iters=self.n_iters, set_temp_zero=True, tag=self._controller._organizer.tag)
+            test_iagent = self._evaluator._arena._add_agent(test_agent, AgentRole.TEST, expand_matrix=True, db=self._evaluator._db)
             with self._lock:
-                gen = EvalUtils.get_next_gen_to_eval(latest_gen, evaluated_gens, target_rating_rate)
-                assert gen is not None
-                conn.aux['gen'] = gen
-
-                test_agent = MCTSAgent(gen, n_iters=self.n_iters, set_temp_zero=True,
-                                       tag=self._controller._organizer.tag)
-                test_iagent = self._evaluator._arena._add_agent(test_agent,
-                                                                AgentRole.TEST,
-                                                                expand_matrix=True,
-                                                                db=self._evaluator._db)
-                self._eval_status_dict[test_iagent.index] = EvalStatus(mcts_gen=gen,
-                                                                       owner=conn.client_id)
-
-            cond = conn.aux['needs_new_opponents_cond']
-            with cond:
-                conn.aux['needs_new_opponents'] = True
-
+                self._eval_status_dict[test_iagent.index] = EvalStatus(mcts_gen=gen, owner=conn.client_id)
+            conn.aux['needs_new_opponents'] = True
             self._set_priority()
         else:
             test_iagent = [iagent for iagent in self._evaluator.indexed_agents if \
                 iagent.role == AgentRole.TEST and iagent.agent.gen == gen][0]
 
-
-        estimated_rating = EvalUtils.estimate_rating_nearby_gens(gen, evaluated_gens, ratings)
+        logger.info('***Evaluating gen %s', gen)
+        estimated_rating = conn.aux['estimated_rating']
         if estimated_rating is None:
-            estimated_rating = np.mean(self._evaluator.arena_ratings)
+            estimated_rating = self._estimate_rating(test_iagent)
+            logger.info('Estimated rating for gen %s: %s', gen, estimated_rating)
+            conn.aux['estimated_rating'] = estimated_rating
 
         n_games_played = self._evaluator._arena.n_games_played(test_iagent.agent)
         n_games_needed = self.n_games - n_games_played
 
-        if conn.aux['needs_new_opponents'] and n_games_needed > 0:
+        need_new_opponents = conn.aux['needs_new_opponents']
+        if need_new_opponents and n_games_needed > 0:
+            logger.info('Requesting new opponents for gen %s, estimated rating: %s', gen, estimated_rating)
             chosen_ixs, num_matches = self._evaluator.gen_matches(test_iagent.agent, estimated_rating, n_games_needed)
-            for ix, match_status in self._eval_status_dict[test_iagent.index].ix_match_status.items():
-                if ix not in chosen_ixs and match_status.status == MatchRequestStatus.PENDING:
-                    match_status.status = MatchRequestStatus.OBSOLETE
-                    logger.info('set match between %s and %s to obsolete', gen, match_status.opponent_gen)
-                elif ix in chosen_ixs and match_status.status == MatchRequestStatus.OBSOLETE:
-                    match_status.status = MatchRequestStatus.PENDING
-                    logger.info('set match between %s and %s to pending', gen, match_status.opponent_gen)
-
-            for ix, n_games in zip(chosen_ixs, num_matches):
-                if ix not in self._eval_status_dict[test_iagent.index].ix_match_status:
-                    new_opponent_gen = self._evaluator.indexed_agents[ix].agent.gen
-                    self._eval_status_dict[test_iagent.index].ix_match_status[ix] = \
-                        MatchStatus(new_opponent_gen, n_games, MatchRequestStatus.PENDING, estimated_rating)
-                    logger.info('add new pending match between %s and %s', gen, new_opponent_gen)
-            cond = conn.aux['needs_new_opponents_cond']
-            with cond:
-                conn.aux['needs_new_opponents'] = False
+            with self._lock:
+                self._update_eval_status(test_iagent.index, chosen_ixs, num_matches)
+            conn.aux['needs_new_opponents'] = False
 
         candidates = [(ix, data.n_games) for ix, data in self._eval_status_dict[test_iagent.index].ix_match_status.items() if data.status == MatchRequestStatus.PENDING]
         next_opponent_ix, next_n_games = sorted(candidates, key=lambda x: x[1])[-1]
@@ -281,6 +256,46 @@ class EvalManager:
             'n_games': int(next_n_games),
         }
         conn.socket.send_json(data)
+
+    def _get_next_gen_to_eval(self):
+        latest_gen = self._controller.latest_gen()
+        evaluated_gens = [data.mcts_gen for data in self._eval_status_dict.values()]
+        target_rating_rate = self._controller.params.target_rating_rate
+        gen = EvalUtils.get_next_gen_to_eval(latest_gen, evaluated_gens, target_rating_rate)
+        return gen
+
+    def _estimate_rating_nearby_gens(self, gen):
+        evaluated_ixs, evaluated_gens = zip(*[(ix, data.mcts_gen) for ix, data in \
+            self._eval_status_dict.items() if data.is_done])
+        ratings = self._evaluator.arena_ratings[np.array(evaluated_ixs)]
+        estimated_rating = EvalUtils.estimate_rating_nearby_gens(gen, evaluated_gens, ratings)
+        return estimated_rating
+
+    def _estimate_rating(self, test_iagent):
+        if self._evaluator._arena.n_games_played(test_iagent.agent) > 0:
+            estimated_rating = self._evaluator.arena_ratings[test_iagent.index]
+        else:
+            estimated_rating = self._estimate_rating_nearby_gens(test_iagent.agent.gen)
+            if estimated_rating is None:
+                estimated_rating = np.mean(self._evaluator.arena_ratings)
+        assert estimated_rating is not None
+        return estimated_rating
+
+    def _update_eval_status(self, test_ix, chosen_ixs, num_matches):
+        for ix, match_status in self._eval_status_dict[test_ix].ix_match_status.items():
+            if ix not in chosen_ixs and match_status.status == MatchRequestStatus.PENDING:
+                match_status.status = MatchRequestStatus.OBSOLETE
+                logger.info('+set match between %s and %s to obsolete', self._evaluator.indexed_agents[test_ix].index, match_status.opponent_gen)
+            elif ix in chosen_ixs and match_status.status == MatchRequestStatus.OBSOLETE:
+                match_status.status = MatchRequestStatus.PENDING
+                logger.info('+set match between %s and %s to pending', self._evaluator.indexed_agents[test_ix].index, match_status.opponent_gen)
+
+        for ix, n_games in zip(chosen_ixs, num_matches):
+            if ix not in self._eval_status_dict[test_ix].ix_match_status:
+                new_opponent_gen = self._evaluator.indexed_agents[ix].agent.gen
+                self._eval_status_dict[test_ix].ix_match_status[ix] = \
+                    MatchStatus(new_opponent_gen, n_games, MatchRequestStatus.PENDING)
+                logger.info('add new pending match between %s and %s', self._evaluator.indexed_agents[test_ix].index, new_opponent_gen)
 
     def _manage_server(self, conn: ClientConnection):
         try:
@@ -328,22 +343,6 @@ class EvalManager:
             logger.warning('eval-server: unknown message type: %s', msg)
         return False
 
-    def _worker_msg_handler(self, conn: ClientConnection, msg: JsonDict) -> bool:
-        msg_type = msg['type']
-        logger.debug('eval-worker received json message: %s', msg)
-
-        if msg_type == 'pause-ack':
-            self._handle_pause_ack(conn)
-        elif msg_type == 'unpause-ack':
-            self._handle_unpause_ack(conn)
-        elif msg_type == 'weights-request':
-            self._handle_weights_request(msg, conn)
-        elif msg_type == 'done':
-            return True
-        else:
-            logger.warning('eval-worker: unknown message type: %s', msg)
-        return False
-
     def _handle_ready(self, conn: ClientConnection):
         status_cond: threading.Condition = conn.aux['status_cond']
         with status_cond:
@@ -351,19 +350,20 @@ class EvalManager:
             status_cond.notify_all()
 
     def _handle_match_result(self, msg: JsonDict, conn: ClientConnection):
-        logger.info('Received match result %s from %s', msg, conn)
         ix1 = msg['ix1']
         ix2 = msg['ix2']
         counts = WinLossDrawCounts.from_json(msg['record'])
+        logger.info('---Received match result for ix1=%s, ix2=%s, counts=%s', ix1, ix2, counts)
         with self._evaluator._db.db_lock:
             self._evaluator._arena.update_match_results(ix1, ix2, counts, MatchType.EVALUATE, self._evaluator._db)
         self._evaluator.refresh_ratings()
         new_rating = self._evaluator.arena_ratings[ix1]
-        old_rating = self._eval_status_dict[ix1].ix_match_status[ix2].estimated_rating
+        old_rating = conn.aux['estimated_rating']
+        logger.info('Old rating: %s, New rating: %s', old_rating, new_rating)
         if abs(new_rating - old_rating) > self.error_threshold:
-            cond = conn.aux['needs_new_opponents_cond']
-            with cond:
-                conn.aux['needs_new_opponents'] = True
+            logger.info('Rating change too large, requesting new opponents...')
+            conn.aux['needs_new_opponents'] = True
+            conn.aux['estimated_rating'] = new_rating
 
         with self._lock:
             self._eval_status_dict[ix1].ix_match_status[ix2].status = MatchRequestStatus.COMPLETE
@@ -381,76 +381,5 @@ class EvalManager:
             with self._evaluator._db.db_lock:
                 self._evaluator._db.commit_ratings(test_iagents, interpolated_ratings)
 
-    def _handle_weights_request(self, msg: JsonDict, conn: ClientConnection):
-        gen = msg['gen']
-        thread = threading.Thread(target=self._manage_worker, args=(gen, conn),
-                                  daemon=True, name=f'manage-eval-worker')
-        thread.start()
-
-    def _manage_worker(self, gen: Generation, conn: ClientConnection):
-        try:
-            domain = conn.client_domain
-            gpu_id = conn.client_gpu_id
-
-            table: GpuContentionTable = self._controller.get_gpu_lock_table(gpu_id)
-            self._pause(conn)
-            self._update_weights(gen, conn)
-
-            while table.active(domain):
-                if not table.acquire_lock(domain):
-                    break
-                self._unpause(conn)
-                if table.wait_for_lock_expiry(domain):
-                    self._pause(conn)
-                    table.release_lock(domain)
-        except SocketSendException:
-            logger.warning('Error sending to %s - worker likely disconnected', conn)
-        except:
-            logger.error('Unexpected error managing %s', conn, exc_info=True)
-            self._controller.request_shutdown(1)
-
-    def _pause(self, conn: ClientConnection):
-        logger.debug('Pausing %s...', conn)
-        data = {
-            'type': 'pause',
-        }
-        conn.aux['pending_pause_ack'] = True
-        conn.socket.send_json(data)
-
-        cond: threading.Condition = conn.aux['ack_cond']
-        with cond:
-            cond.wait_for(lambda: 'pending_pause_ack' not in conn.aux)
-
-        logger.debug('Pause of %s complete!', conn)
-
-    def _unpause(self, conn: ClientConnection):
-        logger.debug('Unpausing %s...', conn)
-        data = {
-            'type': 'unpause',
-        }
-        conn.aux['pending_unpause_ack'] = True
-        conn.socket.send_json(data)
-
-        cond: threading.Condition = conn.aux['ack_cond']
-        with cond:
-            cond.wait_for(lambda: 'pending_unpause_ack' not in conn.aux)
-
-        logger.debug('Unpause of %s complete!', conn)
-
-    def _handle_pause_ack(self, conn: ClientConnection):
-        cond = conn.aux['ack_cond']
-        with cond:
-            conn.aux.pop('pending_pause_ack', None)
-            cond.notify_all()
-
-    def _handle_unpause_ack(self, conn: ClientConnection):
-        cond = conn.aux['ack_cond']
-        with cond:
-            conn.aux.pop('pending_unpause_ack', None)
-            cond.notify_all()
-
-    def _update_weights(self, gen: Generation, conn: ClientConnection):
-        self._controller.broadcast_weights(conn, gen)
-        conn.aux['gen'] = gen
 
 
