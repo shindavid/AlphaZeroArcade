eecbdc96714f8f8ef91f650cf9cac3e050c29472
David Shin <shin.david@gmail.com>
2025-04-06 18:10:55 +0900

    slightly more efficient file transfer mechanics

--- py/alphazero/servers/loop_control/loop_controller.py
+++ py/alphazero/servers/loop_control/loop_controller.py
@@ -280,11 +280,11 @@ class LoopController:
 
     def handle_file_request(self, conn: ClientConnection, files: List[JsonDict]):
         logger.info('Handling file request from %s: %s', conn, files)
+        conn.socket.send_json({
+            'type': 'file-transfer',
+            'files': files,
+        })
         for f in files:
-            conn.socket.send_json({
-                'type': 'file-transfer',
-                'file': f,
-            })
             conn.socket.send_file(f['source_path'])
 
     def set_priority(self, dict_len: int, rating_in_progress: bool):
