83bb16625d07d8dee42e7f7bbf319d55578d1f28
David Shin <shin.david@gmail.com>
2025-04-15 12:28:25 +0900

    Revert "rm unused code"
    This reverts commit 2f691ba7eeac9da0ca1e8515afc8bfdedeff5038.

---
+++ cpp/include/util/AtomicSharedPtr.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+
+namespace util {
+
+/*
+ * Same as std::atomic<std::shared_ptr<T>>, but with various convenience methods to make
+ * AtomicSharedPtr usage similar to a raw pointer.
+ */
+template <typename T>
+class AtomicSharedPtr : public std::atomic<std::shared_ptr<T>> {
+ public:
+  AtomicSharedPtr() = default;
+
+  AtomicSharedPtr(const AtomicSharedPtr& ptr) { this->store(ptr.load()); }
+
+  AtomicSharedPtr& operator=(const AtomicSharedPtr& ptr) {
+    this->store(ptr.load());
+    return *this;
+  }
+
+  AtomicSharedPtr& operator=(const std::shared_ptr<T>& t) {
+    this->store(t);
+    return *this;
+  }
+
+  operator T*() const { return this->load().get(); }
+
+  operator bool() const { return this->load().get() != nullptr; }
+
+  T* operator->() const { return this->load().get(); }
+};
+
+}  // namespace util
