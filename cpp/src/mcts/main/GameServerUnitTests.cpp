f6c4560b7e9d35d159229bfeaf3971106158a17a
Lichen Song <lichen.song.personal@gmail.com>
2025-06-20 17:34:55 -0500

    add -i option

--- cpp/src/mcts/main/GameServerUnitTests.cpp
+++ cpp/src/mcts/main/GameServerUnitTests.cpp
@@ -1,7 +1,11 @@
 #include <core/GameServer.hpp>
 #include <core/PlayerFactory.hpp>
+#include <games/GameTransforms.hpp>
+#include <games/nim/Game.hpp>
 #include <games/tictactoe/Game.hpp>
 #include <games/tictactoe/PlayerFactory.hpp>
+#include <games/stochastic_nim/Game.hpp>
+#include <games/stochastic_nim/PlayerFactory.hpp>
 #include <mcts/SearchLog.hpp>
 #include <util/GTestUtil.hpp>
 
@@ -29,14 +33,15 @@ class GameServerTest : public testing::Test {
     delete search_log_;
   }
 
-  void init_search(const action_vec_t& initial_actions) {
+  void init_search(const action_vec_t& initial_actions, int num_iters) {
     GameServerParams server_params = create_server_params();
     server_params.num_game_threads = 1;  // single-threaded for unit tests
     server_params.num_games = 1;         // run only one game
     TraingDataWriterParams training_data_writer_params;
     server_ = new GameServer(server_params, training_data_writer_params, initial_actions);
 
-    std::string player_str1 = "--type=MCTS-C --name=MCTS --no-model --num-search-thread=1";
+    std::string player_str1 = std::format(
+      "--type=MCTS-C --name=MCTS --no-model --num-search-thread=1 --num-full-iters {}", num_iters);
     std::string player_str2 = "--name=MCTS2 --copy-from=MCTS";
     std::vector<std::string> player_strs = {player_str1, player_str2};
 
@@ -57,22 +62,24 @@ class GameServerTest : public testing::Test {
     });
   }
 
-  void test_search(const std::string& testname, const action_vec_t& initial_actions) {
-    init_search(initial_actions);
+  void test_search(const std::string& testname, int num_iters, const action_vec_t& initial_actions) {
+    init_search(initial_actions, num_iters);
     server_->run();
 
     boost::filesystem::path base_dir = util::Repo::root() / "goldenfiles" / "mcts_tests";
-
-    boost::filesystem::path file_path_graph =
-        base_dir / (testname + "_graph.json");
-
+    boost::filesystem::path file_path_graph = base_dir / (testname + "_graph.json");
     std::ifstream graph_file(file_path_graph);
-
     std::string expected_graph_json((std::istreambuf_iterator<char>(graph_file)),
-                                  std::istreambuf_iterator<char>());
+                                    std::istreambuf_iterator<char>());
     std::stringstream ss;
-    boost_util::pretty_print(ss, search_log_->graphs()[39].graph_repr());
+    boost_util::pretty_print(ss, search_log_->graphs()[num_iters - 1].graph_repr());
     EXPECT_EQ(ss.str(), expected_graph_json);
+
+    if (IS_MACRO_ENABLED(WRITE_LOGFILES)) {
+      boost::filesystem::path log_dir = util::Repo::root() / "sample_search_logs" / "mcts_tests";
+      boost::filesystem::path log_file_path = log_dir / (testname + "_log_debug.json");
+      boost_util::write_str_to_file(search_log_->json_str(), log_file_path);
+    }
   }
 
  private:
@@ -81,10 +88,46 @@ class GameServerTest : public testing::Test {
 };
 
 using TicTacToeTest = GameServerTest<tictactoe::Game, tictactoe::PlayerFactory>;
+using StochasticNimTest = GameServerTest<stochastic_nim::Game, stochastic_nim::PlayerFactory>;
+
+TEST_F(StochasticNimTest, uniform_search) {
+  std::vector<core::action_t> initial_actions = {
+    stochastic_nim::kTake3, 2, stochastic_nim::kTake3, 2, stochastic_nim::kTake3, 1};
+
+  test_search("stochastic_nim_uniform_10", 10, initial_actions);
+}
+
+TEST_F(StochasticNimTest, 20_searches_from_scratch) {
+  test_search("stochastic_nim_uniform", 20, {});
+}
+
+TEST_F(StochasticNimTest, 100_searches_from_4_stones) {
+  std::vector<core::action_t> initial_actions = {
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0,
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0, stochastic_nim::kTake2, 0};
+
+  test_search("stochastic_nim_4_stones", 100, initial_actions);
+}
+
+TEST_F(StochasticNimTest, 100_searches_from_5_stones) {
+  std::vector<core::action_t> initial_actions = {
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0,
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0, stochastic_nim::kTake1, 0};
+
+  test_search("stochastic_nim_5_stones", 100, initial_actions);
+}
+
+TEST_F(StochasticNimTest, 100_searches_from_6_stones) {
+  std::vector<core::action_t> initial_actions = {
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0,
+    stochastic_nim::kTake3, 0, stochastic_nim::kTake3, 0};
+
+  test_search("stochastic_nim_6_stones", 100, initial_actions);
+}
 
 TEST_F(TicTacToeTest, uniform_search) {
   std::vector<core::action_t> initial_actions = {0, 1, 2, 4, 7};
-  test_search("tictactoe_uniform", initial_actions);
+  test_search("tictactoe_uniform", 40, initial_actions);
 }
 
 int main(int argc, char** argv) { return launch_gtest(argc, argv); }
