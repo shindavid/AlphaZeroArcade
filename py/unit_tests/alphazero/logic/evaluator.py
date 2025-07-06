d7148623eaf8f586a4203001308087bf68d24851
David Shin <shin.david@gmail.com>
2025-07-06 06:01:27 +0000

    add simple test of EvalUtils.gen_matches()

---
+++ py/unit_tests/alphazero/logic/evaluator.py
@@ -0,0 +1,30 @@
+from alphazero.logic.evaluator import EvalUtils
+
+import numpy as np
+import unittest
+
+
+class TestEvalUtils(unittest.TestCase):
+    def test_gen_matches(self):
+        np.random.seed(42)
+
+        est_rating = 1150.0
+        ixs = list(range(10))
+        elos = np.array(ixs) * 200
+        n_games = 10000
+        top_k = 5
+
+        chosen_ixs, num_matches = EvalUtils.gen_matches(est_rating, ixs, elos, n_games, top_k)
+
+        counts = dict(zip(chosen_ixs, num_matches))
+        pcts = {int(k): float(v / n_games) for k, v in counts.items()}
+
+        self.assertAlmostEqual(pcts.get(4, -1), 0.13, delta=0.05)
+        self.assertAlmostEqual(pcts.get(5, -1), 0.27, delta=0.05)
+        self.assertAlmostEqual(pcts.get(6, -1), 0.31, delta=0.05)
+        self.assertAlmostEqual(pcts.get(7, -1), 0.19, delta=0.05)
+        self.assertAlmostEqual(pcts.get(8, -1), 0.08, delta=0.05)
+
+
+if __name__ == '__main__':
+    unittest.main()
