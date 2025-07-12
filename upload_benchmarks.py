c21eafc99c72016821de66fb8833b9c7ee7fcfac
Lichen Song <lichen.song.personal@gmail.com>
2025-07-12 15:50:59 -0500

    update auto download in cmake

--- upload_benchmarks.py
+++ upload_benchmarks.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
-from py.util.aws_util import upload_folder
+from alphazero.logic.run_params import RunParams
+from games.game_spec import GameSpec
+from py.util.aws_util import upload_file_to_s3, zip_folder
+from util.py_util import CustomHelpFormatter
 
+import argparse
 import os
 
 
-if __name__ == "__main__":
-    repo_dir = '/workspace/repo/'
-    bucket_name = 'alphazeroarcade'
-    folder_to_upload = os.path.join(repo_dir, 'benchmarks/mcts')
-    s3_prefix = 'benchmarks/mcts'
+def load_args():
+    parser = argparse.ArgumentParser(formatter_class=CustomHelpFormatter)
+    RunParams.add_args(parser)
+    return parser.parse_args()
 
-    upload_folder(folder_to_upload, bucket_name, s3_prefix=s3_prefix)
+
+def main():
+    args = load_args()
+
+    assert args.game is not None, "Must specify --game"
+
+    key = 'benchmarks/mcts'
+    folder_path = os.path.join('/workspace/repo', key)
+    bucket = 'alphazeroarcade'
+    folder_to_upload = os.path.join(folder_path, args.game)
+    zip_file_name = args.game + '.zip'
+    output_zip = os.path.join(folder_path, zip_file_name)
+
+    zip_folder(folder_to_upload, output_zip)
+    upload_file_to_s3(output_zip, bucket, key + '/' + zip_file_name)
+
+
+if __name__ == "__main__":
+    main()
 
