60bbcf5865eab2143a1992173da2cd9104bba581
Lichen Song <lichen.song.personal@gmail.com>
2025-07-12 08:31:57 -0500

    mv function code to aws_utils

--- upload_benchmarks.py
+++ upload_benchmarks.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
+from py.util.aws_util import upload_folder
 
-import boto3
 import os
-from pathlib import Path
-
-
-def upload_folder(folder_path, bucket_name, s3_prefix=""):
-    s3 = boto3.client("s3")
-
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            local_path = os.path.join(root, file)
-            relative_path = os.path.relpath(local_path, folder_path)
-            s3_key = os.path.join(s3_prefix, relative_path)
-
-            print(f"Uploading {local_path} to s3://{bucket_name}/{s3_key}")
-            s3.upload_file(local_path, bucket_name, s3_key)
 
 
 if __name__ == "__main__":
-    REPO_DIR = os.path.join(Path.home(), 'projects/AlphaZeroArcade')
+    repo_dir = '/workspace/repo/'
     bucket_name = 'alphazeroarcade'
-    upload_folder(os.path.join(REPO_DIR, 'benchmarks/mcts'), bucket_name, s3_prefix='mcts')
+    folder_to_upload = os.path.join(repo_dir, 'benchmarks/mcts')
+    s3_prefix = 'benchmarks/mcts'
+
+    upload_folder(folder_to_upload, bucket_name, s3_prefix=s3_prefix)
+
