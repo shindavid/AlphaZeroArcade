ee38ead87036cd7596c4ddb5493347253947f3ac
Lichen Song <lichen.song.personal@gmail.com>
2025-07-20 08:25:30 -0500

    upload works

--- py/alphazero/scripts/upload_benchmark_to_s3.py
+++ py/alphazero/scripts/upload_benchmark_to_s3.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 from alphazero.logic.agent_types import IndexedAgent
 from alphazero.logic.benchmarker import Benchmarker, BenchmarkRatingData
 from alphazero.logic.rating_db import RatingDB
+from alphazero.logic.run_params import RunParams
 from alphazero.servers.loop_control.base_dir import BenchmarkRecord, Workspace
 from alphazero.servers.loop_control.directory_organizer import DirectoryOrganizer
 from util.aws_util import BUCKET
+from util.logging_util import LoggingParams, configure_logger
 from util.py_util import CustomHelpFormatter, tar_and_remotely_copy
 
 import argparse
@@ -67,23 +70,30 @@ def save_benchmark_record(record: BenchmarkRecord):
 
 def load_args():
     parser = argparse.ArgumentParser(formatter_class=CustomHelpFormatter)
-    game_spec: Optional[GameSpec] = RunParams.add_args(parser)
+    RunParams.add_args(parser)
+    LoggingParams.add_args(parser)
+    parser.add_argument('--skip-set-as-default', action='store_true',
+                        help='Skip setting the benchmark as default.')
+    return parser.parse_args()
 
 
 def main():
-    existing_record = Workspace.load_benchmark_record(organizer.game)
-    if existing_record and existing_record.hash == hash:
-        logger.info(f"run at {organizer.base_dir} has the same hash with existing record."
-                    f"Skip updating.")
-    else:
-        utc_key = datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S_UTC')
-        record = BenchmarkRecord(utc_key=utc_key, tag=organizer.tag, game=organizer.game, hash=hash)
-        save_benchmark_data(organizer, record)
-        if not args.skip_set_as_default:
-            save_benchmark_record(record)
-        tar_file = f"{record.data_folder_path()}.tar"
-        tar_and_remotely_copy(record.data_folder_path(), tar_file)
-        BUCKET.upload_file_to_s3(tar_file, record.key())
+    args = load_args()
+    logging_params = LoggingParams.create(args)
+    run_params = RunParams.create(args)
+    configure_logger(params=logging_params, prefix='[upload_benchmark_to_s3]')
+    utc_key = datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S.%f_UTC')
+    record = BenchmarkRecord(utc_key=utc_key, tag=run_params.tag, game=run_params.game)
+
+    organizer = DirectoryOrganizer(RunParams(args.game, args.tag), base_dir_root=Workspace)
+    if not os.path.isdir(organizer.base_dir):
+        raise FileNotFoundError(f"dir {organizer.base_dir} does not exist.")
+    save_benchmark_data(organizer, record)
+    if not args.skip_set_as_default:
+        save_benchmark_record(record)
+    tar_file = f"{record.data_folder_path()}.tar"
+    tar_and_remotely_copy(record.data_folder_path(), tar_file)
+    BUCKET.upload_file_to_s3(tar_file, record.key())
 
 
 if __name__ == '__main__':
