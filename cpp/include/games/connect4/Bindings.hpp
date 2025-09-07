c92b597c68198098fb65b9e8dd389ef777dd1b18
David Shin <shin.david@gmail.com>
2025-09-07 10:48:44 +0900

    split out bindings file

--- cpp/include/games/connect4/Bindings.hpp
+++ cpp/include/games/connect4/Bindings.hpp
@@ -6,45 +6,12 @@
 #include "core/MctsEvalSpec.hpp"
 #include "core/TrainingTargets.hpp"
 #include "games/connect4/Game.hpp"
-#include "util/CppUtil.hpp"
-#include "util/EigenUtil.hpp"
+#include "games/connect4/InputTensorizor.hpp"
 #include "util/MetaProgramming.hpp"
 
 /*
  * Connect4 InputTensorizor
  */
-
-namespace c4 {
-
-struct InputTensorizor {
-  static constexpr int kDim0 = kNumPlayers * (1 + Game::Constants::kNumPreviousStatesToEncode);
-  using Tensor = eigen_util::FTensor<Eigen::Sizes<kDim0, kNumRows, kNumColumns>>;
-
-  template <util::concepts::RandomAccessIteratorOf<Game::State> Iter>
-  static Tensor tensorize(Iter start, Iter cur) {
-    core::seat_index_t cp = Game::Rules::get_current_player(*cur);
-    Tensor tensor;
-    tensor.setZero();
-    int i = 0;
-    Iter state = cur;
-    while (true) {
-      for (int row = 0; row < kNumRows; ++row) {
-        for (int col = 0; col < kNumColumns; ++col) {
-          core::seat_index_t p = state->get_player_at(row, col);
-          if (p < 0) continue;
-          int x = (kNumPlayers + cp - p) % kNumPlayers;
-          tensor(i + x, row, col) = 1;
-        }
-      }
-      if (state == start) break;
-      state--;
-      i += kNumPlayers;
-    }
-    return tensor;
-  }
-};
-
-}  // namespace c4
 
 namespace core {
 
