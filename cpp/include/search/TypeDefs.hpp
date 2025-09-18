455033b128a6f74d2a1b5c0f3978738af7d0870b
David Shin <shin.david@gmail.com>
2025-09-18 13:50:27 +0900

    mv files from nnet/ to search/

--- cpp/include/search/TypeDefs.hpp
+++ cpp/include/search/TypeDefs.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
-#include <memory>
-#include <vector>
+#include "util/CppUtil.hpp"
 
-namespace search {}  // namespace search
+#include <cstdint>
+
+namespace search {
+
+using hash_shard_t = int8_t;
+
+constexpr int kNumHashShardsLog2 = 3;
+constexpr int kNumHashShards = 1 << kNumHashShardsLog2;
+
+constexpr bool kEnableServiceDebug = IS_DEFINED(MCTS_NN_SERVICE_DEBUG);
+
+}  // namespace search
