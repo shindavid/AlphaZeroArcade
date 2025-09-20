7175e432283a942b96323a6e16c0041b3f5063a8
Lichen Song <lichen.song.personal@gmail.com>
2025-09-20 17:35:39 -0500

    update help str for --retrain

--- py/alphazero/servers/loop_control/params.py
+++ py/alphazero/servers/loop_control/params.py
@@ -57,7 +57,7 @@ class LoopControllerParams:
         group.add_argument('--task-mode', action='store_true', default=defaults.task_mode,
                            help='run without self-play/training; exit after task is done.')
         group.add_argument('--retrain', action='store_true', default=False,
-                           help='retrain models from scratch, even if model files are present.')
+                           help='retrain models for a forked run without running self-eval or eval-vs-benchmark.')
 
 
     def add_to_cmd(self, cmd: List[str]):
