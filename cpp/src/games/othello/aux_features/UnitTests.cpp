e8184ca46b64368bd134d36facb6aa9add173207
Lichen Song <lichen.song.personal@gmail.com>
2025-09-25 23:10:14 -0500

    update unit tests

--- cpp/src/games/othello/aux_features/UnitTests.cpp
+++ cpp/src/games/othello/aux_features/UnitTests.cpp
@@ -4,7 +4,7 @@
 
 namespace othello {
 
-TEST(Corner, stable) {
+TEST(StableDiscs, corners) {
   mask_t cur_player_mask = 0x0000000000000011ULL;
   mask_t opponent_mask = 0x0000000000000000ULL;
   mask_t stable = compute_stable_discs(cur_player_mask, opponent_mask);
@@ -16,9 +16,9 @@ TEST(Corner, stable) {
   EXPECT_EQ(stable, 0x8000000000000001ULL);
 
   cur_player_mask = 0x8000000000000011ULL;
-  opponent_mask = 0x7FFFFFFFFFFFFFEFULL;
+  opponent_mask = 0x0100000000000080ULL;
   stable = compute_stable_discs(cur_player_mask, opponent_mask);
-  EXPECT_EQ(stable, 0xFFFFFFFFFFFFFFFFULL);
+  EXPECT_EQ(stable, 0x8100000000000081ULL);
 }
 
 
