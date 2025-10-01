aae4e20d62a74fd8cd520368af0935f292171263
Lichen Song <lichen.song.personal@gmail.com>
2025-10-01 16:57:34 -0500

    move constant building functions out of constants.hpp

--- cpp/include/games/othello/Constants.hpp
+++ cpp/include/games/othello/Constants.hpp
@@ -149,66 +149,6 @@ const core::seat_index_t kBlack = 0;
 const core::seat_index_t kWhite = 1;
 const core::seat_index_t kStartingColor = kBlack;
 
-// -------- Single-line builders --------
-constexpr mask_t rank_mask(int r) {
-  mask_t m = 0;
-  for (int f = 0; f < 8; ++f) m |= detail::bit(f, r);
-  return m;
-}
-
-constexpr mask_t file_mask(int f) {
-  mask_t m = 0;
-  for (int r = 0; r < 8; ++r) m |= detail::bit(f, r);
-  return m;
-}
-
-// from A1 to H8
-constexpr mask_t SE_diag_mask(int f0, int r0) {
-  mask_t m = 0;
-  for (int f = f0, r = r0; detail::in_bounds(f, r); ++f, ++r) m |= detail::bit(f, r);
-  return m;
-}
-
-// from H1 to A8
-constexpr mask_t SW_diag_mask(int f0, int r0) {
-  mask_t m = 0;
-  for (int f = f0, r = r0; detail::in_bounds(f, r); --f, ++r) m |= detail::bit(f, r);
-  return m;
-}
-
-// -------- Array builders --------
-constexpr std::array<mask_t, 8> make_ranks() {
-  std::array<mask_t, 8> a{};
-  for (int r = 0; r < 8; ++r) a[r] = rank_mask(r);
-  return a;
-}
-
-constexpr std::array<mask_t, 8> make_files() {
-  std::array<mask_t, 8> a{};
-  for (int f = 0; f < 8; ++f) a[f] = file_mask(f);
-  return a;
-}
-
-constexpr std::array<mask_t, 15> make_SE_diags() {
-  std::array<mask_t, 15> a{};
-  int k = 0;
-  // starts on bottom row A1..H1
-  for (int f = 0; f < 8; ++f) a[k++] = SE_diag_mask(f, 0);
-  // starts on left column A2..A8
-  for (int r = 1; r < 8; ++r) a[k++] = SE_diag_mask(0, r);
-  return a;
-}
-
-constexpr std::array<mask_t, 15> make_SW_diags() {
-  std::array<mask_t, 15> a{};
-  int k = 0;
-  // starts on bottom row H1..A1
-  for (int f = 7; f >= 0; --f) a[k++] = SW_diag_mask(f, 0);
-  // starts on right column H2..H8
-  for (int r = 1; r < 8; ++r) a[k++] = SW_diag_mask(7, r);
-  return a;
-}
-
 inline constexpr auto kRanks = make_ranks();
 inline constexpr auto kFiles = make_files();
 inline constexpr auto kDiagSE = make_SE_diags();
@@ -218,65 +158,6 @@ static_assert(kRanks[0] == kRank1Mask);
 static_assert(kRanks[7] == kRank8Mask);
 static_assert(kFiles[0] == kFileAMask);
 static_assert(kFiles[7] == kFileHMask);
-
-constexpr uint8_t find_stable_edge(uint8_t old_P, uint8_t old_O, uint8_t stable) {
-  const uint8_t E = uint8_t(~(old_P | old_O));
-  stable = uint8_t(stable & old_P);
-  if (!stable || !E) return stable;
-
-  for (int x = 0; x < 8; ++x)
-    if (E & detail::bit8(x)) {
-      // player plays
-      {
-        uint8_t P = uint8_t(old_P | detail::bit8(x)), O = old_O;
-        if (x > 1) {
-          int y = x - 1;
-          while (y > 0 && (O & detail::bit8(y))) --y;
-          if (P & detail::bit8(y))
-            for (y = x - 1; y > 0 && (O & detail::bit8(y)); --y) {
-              O ^= detail::bit8(y);
-              P ^= detail::bit8(y);
-            }
-        }
-        if (x < 6) {
-          int y = x + 1;
-          while (y < 8 && (O & detail::bit8(y))) ++y;
-          if (P & detail::bit8(y))
-            for (y = x + 1; y < 8 && (O & detail::bit8(y)); ++y) {
-              O ^= detail::bit8(y);
-              P ^= detail::bit8(y);
-            }
-        }
-        stable = find_stable_edge(P, O, stable);
-        if (!stable) return 0;
-      }
-      // opponent plays
-      {
-        uint8_t P = old_P, O = uint8_t(old_O | detail::bit8(x));
-        if (x > 1) {
-          int y = x - 1;
-          while (y > 0 && (P & detail::bit8(y))) --y;
-          if (O & detail::bit8(y))
-            for (y = x - 1; y > 0 && (P & detail::bit8(y)); --y) {
-              O ^= detail::bit8(y);
-              P ^= detail::bit8(y);
-            }
-        }
-        if (x < 6) {
-          int y = x + 1;
-          while (y < 8 && (P & detail::bit8(y))) ++y;
-          if (O & detail::bit8(y))
-            for (y = x + 1; y < 8 && (P & detail::bit8(y)); ++y) {
-              O ^= detail::bit8(y);
-              P ^= detail::bit8(y);
-            }
-        }
-        stable = find_stable_edge(P, O, stable);
-        if (!stable) return 0;
-      }
-    }
-  return stable;
-}
 
 inline constexpr auto EDGE_STABILITY = [] {
   std::array<uint8_t, 256 * 256> t{};
