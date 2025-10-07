7a4a61eec4dc6a87fda14c721afd5b13bb1c9ea2
David Shin <shin.david@gmail.com>
2025-10-07 19:57:25 +0900

    rm stale comment

--- cpp/include/core/concepts/GameSymmetriesConcept.hpp
+++ cpp/include/core/concepts/GameSymmetriesConcept.hpp
@@ -20,16 +20,6 @@ concept OperatesOnWithActionMode =
     { GS::apply(t, action_mode, sym) };
   };
 
-/*
- * One may optionally provide an addition static function:
- *
- * void apply(StateHistory&, group::element_t);
- *
- * Specifying this allows for MCTS optimizations. Without specifying this, MCTS must resort to a
- * "double-pass" through the game-tree on each tree-traversal. We typically expect this overhead to
- * be small compared to the greater cost of neural network inference, but conceivably for some
- * games it could be wise to avoid this if possible.
- */
 template <typename GS, typename GameTypes, typename State>
 concept GameSymmetries = requires(const State& state) {
   { GS::get_mask(state) } -> std::same_as<typename GameTypes::SymmetryMask>;
