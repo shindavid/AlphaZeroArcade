9fae1d9157a9bdf776698f07426bb43cd95eba6d
David Shin <shin.david@gmail.com>
2025-10-13 13:38:31 +0900

    adjust loss weights

--- py/games/connect4/spec.py
+++ py/games/connect4/spec.py
@@ -166,8 +166,8 @@ class CNN_b7_c128_beta0(ModelConfigGenerator):
             BasicLossTerm('policy', 1.0),
             BasicLossTerm('value', 1.5),
             BasicLossTerm('action_value', 1.0),
-            ValueUncertaintyLossTerm('value_uncertainty', 'value', 'Q_min', 'Q_max', 5.0),
-            BasicLossTerm('action_value_uncertainty', 5.0),
+            ValueUncertaintyLossTerm('value_uncertainty', 'value', 'Q_min', 'Q_max', 50.0),
+            BasicLossTerm('action_value_uncertainty', 50.0),
             BasicLossTerm('opp_policy', 0.03),
         ]
 
