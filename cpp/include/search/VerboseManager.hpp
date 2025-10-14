0ecaf0296d78c5b161d0bbf1ea8024266b071250
Lichen Song <lichen.song.personal@gmail.com>
2025-10-14 10:34:13 -0500

    add VerboseManager

---
+++ cpp/include/search/VerboseManager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+# include "search/VerboseDataBase.hpp"
+
+namespace generic {
+
+struct VerboseManager {
+  static VerboseManager& get_instance() {
+    static VerboseManager instance;
+    return instance;
+  }
+
+  void set(VerboseDataBase* verbose_data) { verbose_data_ = verbose_data; }
+  VerboseDataBase* verbose_data() const { return verbose_data_; }
+
+  private:
+    VerboseDataBase* verbose_data_ = nullptr;
+};
+
+}  // namespace generic
