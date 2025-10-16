975cfc8f72878208a981a6798112b3adbe5f1908
Lichen Song <lichen.song.personal@gmail.com>
2025-10-15 22:50:41 -0500

    make to_terminal_text with no arguments

--- cpp/include/search/VerboseDataBase.hpp
+++ cpp/include/search/VerboseDataBase.hpp
@@ -7,7 +7,7 @@ namespace generic {
 struct VerboseDataBase {
   virtual ~VerboseDataBase() {}
   virtual boost::json::object to_json() const = 0;
-  virtual void to_terminal_text(std::ostream& ss, int n_rows_to_display) const = 0;
+  virtual void to_terminal_text() const = 0;
 
   bool initialized = false;
 };
