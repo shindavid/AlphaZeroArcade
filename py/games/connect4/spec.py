f9a1fed89b7dc8fbb4edaccaf243b2b8ad98bc43
David Shin <shin.david@gmail.com>
2025-10-23 14:03:55 +0900

    fix bad assert

--- py/games/connect4/spec.py
+++ py/games/connect4/spec.py
@@ -100,7 +100,7 @@ class CNN_b7_c128_beta0(ModelConfigGenerator):
 
         assert value_shape == (3,), value_shape
         assert value_uncertainty_shape == (2,), value_uncertainty_shape
-        assert action_value_uncertainty_shape == (7,), action_value_uncertainty_shape
+        assert action_value_uncertainty_shape == (7, 2), action_value_uncertainty_shape
 
         c_trunk = 128
         c_mid = 128
