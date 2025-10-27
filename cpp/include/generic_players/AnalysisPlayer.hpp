885a1385fc06ae3982be1559be7f3577f72f1502
Lichen Song <lichen.song.personal@gmail.com>
2025-10-26 20:58:41 -0500

    write out AnalysisPlayer as a wrapper that does nothing

---
+++ cpp/include/generic_players/AnalysisPlayer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "core/AbstractPlayer.hpp"
+#include "core/concepts/GameConcept.hpp"
+
+namespace generic {
+
+template <core::concepts::Game Game>
+class AnalysisPlayer : public core::AbstractPlayer<Game> {
+ public:
+  using State = typename Game::State;
+  using ActionRequest = typename Game::Types::ActionRequest;
+  using ActionResponse = typename Game::Types::ActionResponse;
+  using GameResultTensor = typename Game::Types::GameResultTensor;
+
+  AnalysisPlayer(core::AbstractPlayer<Game>* wrapped_player) : wrapped_player_(wrapped_player) {}
+  ~AnalysisPlayer() { delete wrapped_player_; }
+
+  bool start_game() override { return wrapped_player_->start_game(); }
+  void receive_state_change(core::seat_index_t seat, const State& state,
+                            core::action_t action) override {
+    wrapped_player_->receive_state_change(seat, state, action);
+  }
+  ActionResponse get_action_response(const ActionRequest& request) override {
+    return wrapped_player_->get_action_response(request);
+  }
+  void end_game(const State& state, const GameResultTensor& outcome) override {
+    wrapped_player_->end_game(state, outcome);
+  }
+
+  bool disable_progress_bar() const override { return true; }
+
+ private:
+  core::AbstractPlayer<Game>* wrapped_player_;
+};
+
+}  // namespace generic
