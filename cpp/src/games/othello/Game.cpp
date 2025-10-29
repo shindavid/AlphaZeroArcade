8fc0577234eff93d01128916bc739ffefb3c7413
Lichen Song <lichen.song.personal@gmail.com>
2025-10-29 17:35:55 -0500

    add seat_char_to_index in IO

--- cpp/src/games/othello/Game.cpp
+++ cpp/src/games/othello/Game.cpp
@@ -207,4 +207,13 @@ boost::json::value Game::IO::state_to_json(const State& state) {
   return buf;
 }
 
+int Game::IO::seat_char_to_index(char seat_char) {
+  for (int i = 0; i < Constants::kNumPlayers; ++i) {
+    if (Constants::kSeatChars[i] == seat_char) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 }  // namespace othello
