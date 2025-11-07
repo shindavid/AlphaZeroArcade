7cfc3e1b380c966c5973b106e69904078879166b
Lichen Song <lichen.song.personal@gmail.com>
2025-11-06 22:33:22 -0600

    rm c4 WebPlayer.hpp

--- cpp/include/games/connect4/PlayerFactory.hpp
+++ cpp/include/games/connect4/PlayerFactory.hpp
@@ -7,8 +7,8 @@
 #include "games/connect4/Game.hpp"
 #include "games/connect4/players/HumanTuiPlayerGenerator.hpp"
 #include "games/connect4/players/PerfectPlayerGenerator.hpp"
-#include "games/connect4/players/WebPlayer.hpp"
 #include "generic_players/RandomPlayerGenerator.hpp"
+#include "generic_players/WebPlayer.hpp"
 #include "generic_players/WebPlayerGenerator.hpp"
 #include "generic_players/alpha0/PlayerGenerator.hpp"
 #include "generic_players/beta0/PlayerGenerator.hpp"
@@ -33,7 +33,7 @@ class PlayerFactory : public core::PlayerFactory<Game> {
       new core::PlayerSubfactory<generic::beta0::CompetitionPlayerGenerator<BetaZeroTraits>>(),
       new core::PlayerSubfactory<generic::beta0::TrainingPlayerGenerator<BetaZeroTraits>>(),
       new core::PlayerSubfactory<c4::PerfectPlayerGenerator>(),
-      new core::PlayerSubfactory<generic::WebPlayerGenerator<c4::WebPlayer>>(),
+      new core::PlayerSubfactory<generic::WebPlayerGenerator<generic::WebPlayer<Game>>>(),
       new core::PlayerSubfactory<generic::RandomPlayerGenerator<Game>>(),
       new core::PlayerSubfactory<core::RemotePlayerProxyGenerator<Game>>()};
   }
