1d77eb6600839920c4b721a5426068ccb71ca940
David Shin <shin.david@gmail.com>
2025-12-19 17:09:37 +0900

    shuffle around SearchResults fields

--- cpp/include/beta0/SearchResults.hpp
+++ cpp/include/beta0/SearchResults.hpp
@@ -1,37 +1,23 @@
 #pragma once
 
 #include "core/concepts/GameConcept.hpp"
+#include "x0/SearchResults.hpp"
 
 #include <boost/json.hpp>
 
 namespace beta0 {
 
 template <core::concepts::Game Game>
-struct SearchResults {
-  using ActionMask = Game::Types::ActionMask;
-  using ActionSymmetryTable = Game::Types::ActionSymmetryTable;
+struct SearchResults : public x0::SearchResults<Game> {
   using ActionValueTensor = Game::Types::ActionValueTensor;
-  using PolicyTensor = Game::Types::PolicyTensor;
   using ValueArray = Game::Types::ValueArray;
-  using GameResultTensor = Game::Types::GameResultTensor;
 
-  ActionMask valid_actions;
-
-  PolicyTensor P;
-  PolicyTensor pi;
   ActionValueTensor AQ;
   ActionValueTensor AW;
 
-  GameResultTensor R;
-  ValueArray Q;
   ValueArray Q_min;
   ValueArray Q_max;
   ValueArray W;
-
-  ActionSymmetryTable action_symmetry_table;
-  core::action_mode_t action_mode;
-  bool trivial;  // all actions are symmetrically equivalent
-  bool provably_lost = false;
 
   boost::json::object to_json() const;
 };
