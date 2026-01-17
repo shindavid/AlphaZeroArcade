9b0c35c938013213da060f2dc59e4e38c086f672
David Shin <shin.david@gmail.com>
2026-01-17 13:05:42 +0900

    othello beta0

--- cpp/include/games/othello/PlayerFactory.hpp
+++ cpp/include/games/othello/PlayerFactory.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "alpha0/Traits.hpp"
+#include "beta0/Traits.hpp"
 #include "core/PlayerFactory.hpp"
 #include "core/players/RemotePlayerProxyGenerator.hpp"
 #include "games/othello/Game.hpp"
 #include "games/othello/players/EdaxPlayerGenerator.hpp"
 #include "games/othello/players/HumanTuiPlayerGenerator.hpp"
 #include "generic_players/RandomPlayerGenerator.hpp"
+#include "generic_players/WebPlayer.hpp"
 #include "generic_players/WebPlayerGenerator.hpp"
 #include "generic_players/alpha0/PlayerGenerator.hpp"
+#include "generic_players/beta0/PlayerGenerator.hpp"
 
 namespace othello {
 
@@ -17,6 +20,7 @@ class PlayerFactory : public core::PlayerFactory<Game> {
   using base_t = core::PlayerFactory<Game>;
   using player_subfactory_vec_t = base_t::player_subfactory_vec_t;
   using AlphaZeroTraits = ::alpha0::Traits<Game>;
+  using BetaZeroTraits = ::beta0::Traits<Game>;
 
   PlayerFactory() : base_t(make_subfactories()) {}
 
@@ -28,6 +32,8 @@ class PlayerFactory : public core::PlayerFactory<Game> {
       new core::PlayerSubfactory<generic::WebPlayerGenerator<generic::WebPlayer<Game>>>(),
       new core::PlayerSubfactory<generic::alpha0::CompetitionPlayerGenerator<AlphaZeroTraits>>(),
       new core::PlayerSubfactory<generic::alpha0::TrainingPlayerGenerator<AlphaZeroTraits>>(),
+      new core::PlayerSubfactory<generic::beta0::CompetitionPlayerGenerator<BetaZeroTraits>>(),
+      new core::PlayerSubfactory<generic::beta0::TrainingPlayerGenerator<BetaZeroTraits>>(),
       new core::PlayerSubfactory<generic::RandomPlayerGenerator<Game>>(),
       new core::PlayerSubfactory<core::RemotePlayerProxyGenerator<Game>>()};
   }
