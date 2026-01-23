25c9aa6684864ce404fef9afc1080356f686789e
Lichen Song <lichen.song.personal@gmail.com>
2026-01-23 06:41:57 +0000

    add virtual destructor to x0::AuxData

--- cpp/include/x0/AuxData.hpp
+++ cpp/include/x0/AuxData.hpp
@@ -6,6 +6,9 @@ namespace x0 {
 
 struct AuxData {
   core::ActionResponse action_response;
+
+  AuxData(const core::ActionResponse& ar) : action_response(ar) {}
+  virtual ~AuxData() = default;
 };
 
 }  // namespace x0
