2a9f30bafcea579a91bbb009b382ce7f3b1d76f7
Lichen Song <lichen.song.personal@gmail.com>
2026-01-28 14:54:06 +0000

    rm apply(action)

--- cpp/include/games/hex/Game.hpp
+++ cpp/include/games/hex/Game.hpp
@@ -28,7 +28,6 @@ struct Game {
     static void apply(State& state, group::element_t sym);
     template <eigen_util::concepts::FTensor Tensor>
     static void apply(Tensor& tensor, group::element_t sym, core::action_mode_t);
-    static void apply(core::action_t& action, group::element_t sym, core::action_mode_t);
     static group::element_t get_canonical_symmetry(const State& state);
   };
 
