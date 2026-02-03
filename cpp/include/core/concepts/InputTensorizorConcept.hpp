4f10d1c1ba6647df0bfc360f482a7a22098e26b6
Lichen Song <lichen.song.personal@gmail.com>
2026-02-03 03:34:16 +0000

    rm update() from concept

--- cpp/include/core/concepts/InputTensorizorConcept.hpp
+++ cpp/include/core/concepts/InputTensorizorConcept.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "core/BasicTypes.hpp"
 #include "core/concepts/KeysConcept.hpp"
 #include "util/CppUtil.hpp"
 #include "util/EigenUtil.hpp"
@@ -10,24 +11,24 @@
 namespace core::concepts {
 
 template <typename IT, typename Game>
-concept InputTensorizor =
-  requires(group::element_t sym, typename Game::State state, typename IT::StateIterator it) {
-    typename IT::Tensor;
-    typename IT::Keys;
+concept InputTensorizor = requires(group::element_t sym, typename Game::State state,
+                                   typename IT::StateIterator it, core::action_t action) {
+  typename IT::Tensor;
+  typename IT::Keys;
 
-    requires eigen_util::concepts::FTensor<typename IT::Tensor>;
-    requires core::concepts::Keys<typename IT::Keys, Game>;
+  requires eigen_util::concepts::FTensor<typename IT::Tensor>;
+  requires core::concepts::Keys<typename IT::Keys, Game>;
 
-    // kNumStatesToEncode is the number of State's that are needed to tensorize a given state. If
-    // the neural network does not need any previous State's, kNumStatesToEncode should be 1.
-    { util::decay_copy(IT::kNumStatesToEncode) } -> std::same_as<int>;
+  // kNumStatesToEncode is the number of State's that are needed to tensorize a given state. If
+  // the neural network does not need any previous State's, kNumStatesToEncode should be 1.
+  { util::decay_copy(IT::kNumStatesToEncode) } -> std::same_as<int>;
 
-    { IT::tensorize(sym) } -> std::same_as<typename IT::Tensor>;
-    { IT::get_random_symmetry() } -> std::same_as<group::element_t>;
-    { IT::update(state) } -> std::same_as<void>;
-    { IT::undo(state) } -> std::same_as<void>;
-    { IT::jump_to(it) } -> std::same_as<void>;
-    { IT::clear() } -> std::same_as<void>;
-  };
+  { IT::tensorize(sym) } -> std::same_as<typename IT::Tensor>;
+  { IT::get_random_symmetry() } -> std::same_as<group::element_t>;
+  { IT::apply_action(action) } -> std::same_as<void>;
+  { IT::undo(state) } -> std::same_as<void>;
+  { IT::jump_to(it) } -> std::same_as<void>;
+  { IT::clear() } -> std::same_as<void>;
+};
 
 }  // namespace core::concepts
