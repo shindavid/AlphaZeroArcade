b327c568fb90ce4f4352e215b8127c63de4ff11f
Lichen Song <lichen.song.personal@gmail.com>
2026-02-05 23:52:06 +0000

    add docstring for lc0

--- cpp/include/games/chess/InputTensorizor.hpp
+++ cpp/include/games/chess/InputTensorizor.hpp
@@ -7,14 +7,22 @@
 
 namespace chess {
 
+/*
+ * InputTensorizor implements the "Classical" input feature set used by Leela Chess Zero (Lc0).
+ * * Dimensions: [112, 8, 8]
+ * - History: 8 time steps (Current + 7 past)
+ * - Planes per step: 13 (6 Us, 6 Them, 1 Repetitions)
+ * - Aux planes: 8 (Castling, Ep, Rule50, etc.)
+ *
+ * Source:
+ * https://github.com/LeelaChessZero/lc0/blob/master/src/neural/encoder.h
+ * https://github.com/LeelaChessZero/lc0/blob/master/src/neural/encoder.cc
+ */
+
 struct InputTensorizor : public core::MultiStateInputTensorizorBase<Game, kNumPastStatesToEncode> {
-  // +1 for current state
   static constexpr int kNumStatesToEncode = kNumPastStatesToEncode + 1;
-  // 6 (Us pieces) + 6 (Them pieces) + 1 (repitition) = 13
   static constexpr int kPlanesPerStep = 13;
-  // Castling (4) + En Passant (1) + Side to Move (1) + Rule50 (1) + Zero/Constant (1) = 8
   static constexpr int kAuxiliaryPlanes = 8;
-  // kDim0 = 112
   static constexpr int kDim0 = kPlanesPerStep * kNumStatesToEncode + kAuxiliaryPlanes;
   using Tensor = eigen_util::FTensor<Eigen::Sizes<kDim0, kBoardDim, kBoardDim>>;
 
