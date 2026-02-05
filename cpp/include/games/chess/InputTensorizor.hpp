78cacf6040f3f98f9908af4dc8bd5687d842e136
Lichen Song <lichen.song.personal@gmail.com>
2026-02-05 23:42:39 +0000

    chess tensor dim0

--- cpp/include/games/chess/InputTensorizor.hpp
+++ cpp/include/games/chess/InputTensorizor.hpp
@@ -8,7 +8,14 @@
 namespace chess {
 
 struct InputTensorizor : public core::MultiStateInputTensorizorBase<Game, kNumPastStatesToEncode> {
-  static constexpr int kDim0 = kNumPlayers * kNumStatesToEncode;
+  // +1 for current state
+  static constexpr int kNumStatesToEncode = kNumPastStatesToEncode + 1;
+  // 6 (Us pieces) + 6 (Them pieces) + 1 (repitition) = 13
+  static constexpr int kPlanesPerStep = 13;
+  // Castling (4) + En Passant (1) + Side to Move (1) + Rule50 (1) + Zero/Constant (1) = 8
+  static constexpr int kAuxiliaryPlanes = 8;
+  // kDim0 = 112
+  static constexpr int kDim0 = kPlanesPerStep * kNumStatesToEncode + kAuxiliaryPlanes;
   using Tensor = eigen_util::FTensor<Eigen::Sizes<kDim0, kBoardDim, kBoardDim>>;
 
   inline Tensor tensorize(group::element_t sym = group::kIdentity) {
