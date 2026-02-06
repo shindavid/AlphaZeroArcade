51d286e120e5d51e732aeafc32811d814de6531e
Lichen Song <lichen.song.personal@gmail.com>
2026-02-06 02:38:00 +0000

    parse log for paradigm

--- py/tools/one_off/create_default_version_files.py
+++ py/tools/one_off/create_default_version_files.py
@@ -9,8 +9,35 @@ from alphazero.servers.loop_control.base_dir import Workspace
 from alphazero.servers.loop_control.directory_organizer import DirectoryOrganizer
 
 from pathlib import Path
+import re
 
+def parse_for_paradigm(log_file: Path) -> str:
+    try:
+        with open(log_file, 'r') as f:
+            content = f.read()
+            match = re.search(r'--type\s+([^\s-]+)', content)
+        if match:
+            return match.group(1)
+        return None
 
+    except Exception as e:
+        print(f"Error reading {log_file}: {e}")
+
+def create_version_file(organizer):
+    version_file = Path(organizer.version_filename)
+    if not version_file.exists():
+        log_dir = Path(organizer.logs_dir) / 'self-play-server'
+        for entry in log_dir.iterdir():
+            if not entry.is_file():
+                continue
+
+            paradigm = parse_for_paradigm(entry)
+            if paradigm:
+                organizer.write_version_file(paradigm)
+                print(f"Created version file for {organizer.game}/{organizer.tag} with paradigm {paradigm}")
+                return
+
+        print(f"Failed to determine paradigm for {organizer.game}/{organizer.tag}.")
 def main():
     output_dir = Path(Workspace.output_dir())
 
@@ -22,10 +49,7 @@ def main():
             if not dir.is_dir():
                 continue
             organizer = DirectoryOrganizer(RunParams(game=game_dir.name, tag=dir.name), Workspace)
-            version_file = Path(organizer.version_filename)
-            if not version_file.exists():
-                print(f"Creating version file for {game_dir.name}/{dir.name}...")
-                organizer.write_version_file('alpha0')
+            create_version_file(organizer)
 
 if __name__ == '__main__':
     main()
