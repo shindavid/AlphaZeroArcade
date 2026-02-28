dac1b461aab0bb7166c2f7ab6c67787a13acba3f
Lichen Song <lichen.song.personal@gmail.com>
2026-02-28 00:17:05 +0000

    Reapply "rm prev_states_ in board.hpp"
    This reverts commit d189d62863f72b2cda0e773e90460dd38eb24996.

--- extra_deps/chess-library/src/board.hpp
+++ extra_deps/chess-library/src/board.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "util/StaticCircularBuffer.hpp"
-
 #include <array>
 #include <cassert>
 #include <cctype>
@@ -133,23 +131,6 @@ class Board {
     };
 
    private:
-    struct State {
-        U64 hash;
-        CastlingRights castling;
-        Square enpassant;
-        std::uint8_t half_moves;
-        Piece captured_piece;
-
-        State() = default;
-
-        State(const U64& hash, const CastlingRights& castling, const Square& enpassant, const std::uint8_t& half_moves,
-              const Piece& captured_piece)
-            : hash(hash),
-              castling(castling),
-              enpassant(enpassant),
-              half_moves(half_moves),
-              captured_piece(captured_piece) {}
-    };
 
     enum class PrivateCtor { CREATE };
 
@@ -298,8 +279,6 @@ class Board {
 
         // Validate side to move
         assert((at(move.from()) < Piece::BLACKPAWN) == (stm_ == Color::WHITE));
-
-        prev_states_.push_back(State(key_, cr_, ep_sq_, hfm_, captured));
 
         hfm_++;
         plies_++;
@@ -575,28 +554,6 @@ class Board {
     }
 
     /**
-     * @brief Checks if the current position is a repetition, set this to 1 if
-     * you are writing a chess engine.
-     * @param count
-     * @return
-     */
-    [[nodiscard]] bool isRepetition(int count = 2) const noexcept {
-        std::uint8_t c = 0;
-
-        // We start the loop from the back and go forward in moves, at most to the
-        // last move which reset the half-move counter because repetitions cant
-        // be across half-moves.
-        const auto size = static_cast<int>(prev_states_.size());
-
-        for (int i = size - 2; i >= 0 && i >= size - hfm_ - 1; i -= 2) {
-            if ((prev_states_.begin() + i)->hash == key_) c++;
-            if (c == count) return true;
-        }
-
-        return false;
-    }
-
-    /**
      * @brief Checks if the current position is a draw by 50 move rule.
      * Keep in mind that by the rules of chess, if the position has 50 half
      * moves it's not necessarily a draw, since checkmate has higher priority,
@@ -656,28 +613,6 @@ class Board {
         }
 
         return false;
-    }
-
-    /**
-     * @brief Checks if the game is over. Returns GameResultReason::NONE if the game is not over.
-     * This function calculates all legal moves for the current position to check if the game is over.
-     * If you are writing a chess engine you should not use this function.
-     * @return
-     */
-    [[nodiscard]] std::pair<GameResultReason, GameResult> isGameOver() const noexcept {
-        if (isHalfMoveDraw()) return getHalfMoveDrawType();
-        if (isInsufficientMaterial()) return {GameResultReason::INSUFFICIENT_MATERIAL, GameResult::DRAW};
-        if (isRepetition()) return {GameResultReason::THREEFOLD_REPETITION, GameResult::DRAW};
-
-        Movelist movelist;
-        movegen::legalmoves(movelist, *this);
-
-        if (movelist.empty()) {
-            if (inCheck()) return {GameResultReason::CHECKMATE, GameResult::LOSE};
-            return {GameResultReason::STALEMATE, GameResult::DRAW};
-        }
-
-        return {GameResultReason::NONE, GameResult::NONE};
     }
 
     /**
@@ -874,8 +809,6 @@ class Board {
     void placePiece(Piece piece, Square sq) { placePieceInternal(piece, sq); }
 
     void removePiece(Piece piece, Square sq) { removePieceInternal(piece, sq); }
-
-    util::StaticCircularBuffer<State, 128> prev_states_;
 
     std::array<Bitboard, 6> pieces_bb_ = {};
     std::array<Bitboard, 2> occ_bb_    = {};
@@ -1285,7 +1218,6 @@ class Board {
         plies_ = 1;
         key_   = 0ULL;
         cr_.clear();
-        prev_states_.clear();
     }
 };
 
