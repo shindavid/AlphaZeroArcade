45c64fd2fba57736d56572f67a353752137acda7
Lichen Song <lichen.song.personal@gmail.com>
2026-04-06 19:25:05 -0500

    phase

--- cpp/include/games/stochastic_nim/PolicyEncoding.hpp
+++ cpp/include/games/stochastic_nim/PolicyEncoding.hpp
@@ -16,7 +16,7 @@ struct PolicyEncoding {
 
   static Index to_index(const InputFrame&, const Move& move) { return Index{move.index()}; }
   static Move to_move(const State& state, const Index& index) {
-    return Move(index[0], state.current_phase);
+    return Move(index[0], state.phase);
   }
 };
 
